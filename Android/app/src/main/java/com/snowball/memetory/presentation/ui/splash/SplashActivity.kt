dda9ac8fcfb075398508e3963411187338398854
youngseo3 <wys4475@gmail.com>
2024-01-29 22:29:34 +0900

    [#6] Splash UI 구현

---
+++ Android/app/src/main/java/com/snowball/memetory/presentation/ui/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.snowball.memetory.presentation.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.snowball.memetory.R
+import com.snowball.memetory.presentation.ui.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+
+        }, 1000)
+    }
+}
