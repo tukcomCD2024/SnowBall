12d4019045b9361e28beba29ec5e297fca7d41f3
JunRain <sa2313@tukorea.ac.kr>
2024-01-30 16:25:14 +0900

    feat#8/이미지 이름을 유니크하게 저장

--- AI/roop/flask_server/app.py
+++ AI/roop/flask_server/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
+import hashlib
+import time
 
 app = Flask(__name__)
 
@@ -11,9 +13,17 @@ def upload_file():
         # target 파일이 바탕화면
         target_file = request.files['targetFile']
         source_file = request.files['sourceFile']
-        # 저장할 경로 + 파일명
-        target_file.save('../image/' + secure_filename(target_file.filename))
-        source_file.save('../image/' + secure_filename(source_file.filename))
+
+        # 파일명과 현재 시간을 결합하여 고유한 해시 생성
+        hash_input = target_file.filename + str(time.time())
+        target_filename = hashlib.sha256(hash_input.encode()).hexdigest() + '_' + secure_filename(target_file.filename)
+
+        hash_input = source_file.filename + str(time.time())
+        source_filename = hashlib.sha256(hash_input.encode()).hexdigest() + '_' + secure_filename(source_file.filename)
+
+        # 저장할 경로 + 고유한 파일명
+        target_file.save('../image/' + target_filename)
+        source_file.save('../image/' + source_filename)
         return 'uploads 디렉토리 -> 파일 업로드 성공!'
 
 
