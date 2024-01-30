8948c7a602949847ea8132052a388dec6924bfd4
JunRain <sa2313@tukorea.ac.kr>
2024-01-30 16:27:36 +0900

    feat#8/file에서 image로 명시적으로 이름 변경

--- AI/roop/flask_server/app.py
+++ AI/roop/flask_server/app.py
@@ -11,19 +11,19 @@ app = Flask(__name__)
 def upload_file():
     if request.method == 'POST':
         # target 파일이 바탕화면
-        target_file = request.files['targetFile']
-        source_file = request.files['sourceFile']
+        target_image = request.files['targetImage']
+        source_image = request.files['sourceImage']
 
         # 파일명과 현재 시간을 결합하여 고유한 해시 생성
-        hash_input = target_file.filename + str(time.time())
-        target_filename = hashlib.sha256(hash_input.encode()).hexdigest() + '_' + secure_filename(target_file.filename)
+        hash_input = target_image.filename + str(time.time())
+        target_image_name = hashlib.sha256(hash_input.encode()).hexdigest() + '_' + secure_filename(target_image.filename)
 
-        hash_input = source_file.filename + str(time.time())
-        source_filename = hashlib.sha256(hash_input.encode()).hexdigest() + '_' + secure_filename(source_file.filename)
+        hash_input = source_image.filename + str(time.time())
+        source_image_name = hashlib.sha256(hash_input.encode()).hexdigest() + '_' + secure_filename(source_image.filename)
 
         # 저장할 경로 + 고유한 파일명
-        target_file.save('../image/' + target_filename)
-        source_file.save('../image/' + source_filename)
+        target_image.save('../image/' + target_image_name)
+        source_image.save('../image/' + source_image_name)
         return 'uploads 디렉토리 -> 파일 업로드 성공!'
 
 
