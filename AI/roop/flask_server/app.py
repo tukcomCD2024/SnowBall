8065c484d14137d164bca6ee681df9cb896aaf39
JunRain <sa2313@tukorea.ac.kr>
2024-01-30 14:57:14 +0900

    feat#8/flask 서버 이미지 업로드 구현

---
+++ AI/roop/flask_server/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+
+# 파일 업로드 처리
+@app.route('/fileUpload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # target 파일이 바탕화면
+        target_file = request.files['targetFile']
+        source_file = request.files['sourceFile']
+        # 저장할 경로 + 파일명
+        target_file.save('/Users/junrain/Downloads/server/' + secure_filename(target_file.filename))
+        source_file.save('/Users/junrain/Downloads/server/' + secure_filename(source_file.filename))
+        return 'uploads 디렉토리 -> 파일 업로드 성공!'
