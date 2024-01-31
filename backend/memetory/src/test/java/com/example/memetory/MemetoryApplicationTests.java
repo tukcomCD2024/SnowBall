8474332c1beef45acc1304f37722e72ddde8a281
ggamD00 <jaehuek@naver.com>
2024-01-31 21:57:14 +0900

    chore: spring boot 생성

---
+++ backend/memetory/src/test/java/com/example/memetory/MemetoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memetory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemetoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
