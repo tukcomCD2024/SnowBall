f17af07e30e9d52cffcb2b75963e682ac14b25d5
JunRain <sa2313@tukorea.ac.kr>
2024-01-31 14:30:42 +0900

    feat#8/저장되는 이미지 명을 4글자로 변경

--- AI/roop/flask_server/app.py
+++ AI/roop/flask_server/app.py
@@ -17,13 +17,13 @@ def upload_file():
         target_image = request.files['targetImage']
         source_image = request.files['sourceImage']
 
-        # 파일명과 현재 시간을 결합하여 고유한 해시 생성
+        # 파일 확장자와 현재 시간을 결합하여 고유한 해시 생성
         hash_input = target_image.filename + str(time.time())
-        target_image_name = hashlib.sha256(hash_input.encode()).hexdigest() + "." + secure_filename(
+        target_image_name = hashlib.sha256(hash_input.encode()).hexdigest()[:4] + "." + secure_filename(
             os.path.splitext(target_image.filename)[1])
 
         hash_input = source_image.filename + str(time.time())
-        source_image_name = hashlib.sha256(hash_input.encode()).hexdigest() + "." + secure_filename(
+        source_image_name = hashlib.sha256(hash_input.encode()).hexdigest()[:4] + "." + secure_filename(
             os.path.splitext(source_image.filename)[1])
 
         # 저장할 경로 + 고유한 파일명
