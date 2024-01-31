4c99502067309da1dfbfe9e1aa7412537518e230
JunRain <sa2313@tukorea.ac.kr>
2024-01-31 14:48:36 +0900

    feat#8/face-swap 메서드 분리

--- AI/roop/flask_server/app.py
+++ AI/roop/flask_server/app.py
@@ -17,35 +17,45 @@ def upload_file():
         target_image = request.files['targetImage']
         source_image = request.files['sourceImage']
 
-        # 파일 확장자와 현재 시간을 결합하여 고유한 해시 생성
-        hash_input = target_image.filename + str(time.time())
-        target_image_name = hashlib.sha256(hash_input.encode()).hexdigest()[:4] + "." + secure_filename(
-            os.path.splitext(target_image.filename)[1])
-
-        hash_input = source_image.filename + str(time.time())
-        source_image_name = hashlib.sha256(hash_input.encode()).hexdigest()[:4] + "." + secure_filename(
-            os.path.splitext(source_image.filename)[1])
-
-        # 저장할 경로 + 고유한 파일명
-        target_image.save('../image/' + target_image_name)
-        source_image.save('../image/' + source_image_name)
-
-        command = [
-            "python",
-            "../run.py",
-            "-s",
-            "../image/" + source_image_name,
-            "-t",
-            '../image/' + target_image_name,
-            "-o",
-            "../image/faceswap_image/",
-            "--frame-processor",
-            "face_swapper"
-        ]
-
-        subprocess.run(command)
+        print(file_swap(target_image, source_image))
 
         return 'uploads 디렉토리 -> 파일 업로드 성공!'
+
+
+def file_swap(target_image, source_image):
+    # 파일 확장자와 현재 시간을 결합하여 고유한 해시 생성
+    hash_input = target_image.filename + str(time.time())
+    target_image_extension = secure_filename(os.path.splitext(target_image.filename)[1])
+    target_image_name = hashlib.sha256(hash_input.encode()).hexdigest()[:4]
+    target_image_all_name = target_image_name + "." + target_image_extension
+
+    hash_input = source_image.filename + str(time.time())
+    source_image_extension = secure_filename(os.path.splitext(source_image.filename)[1])
+    source_image_name = hashlib.sha256(hash_input.encode()).hexdigest()[:4]
+    source_image_all_name = source_image_name + "." + source_image_extension
+
+    # 저장할 경로 + 고유한 파일명
+    target_image.save('../image/' + target_image_all_name)
+    source_image.save('../image/' + source_image_all_name)
+
+    command = [
+        "python",
+        "../run.py",
+        "-s",
+        "../image/" + source_image_all_name,
+        "-t",
+        '../image/' + target_image_all_name,
+        "-o",
+        "../image/faceswap_image/",
+        "--frame-processor",
+        "face_swapper"
+    ]
+
+    subprocess.run(command)
+
+    face_swap_image_path = "../image/faceswap_image/" + source_image_name + "-" + target_image_all_name
+
+    return face_swap_image_path
 
 
 if __name__ == '__main__':
