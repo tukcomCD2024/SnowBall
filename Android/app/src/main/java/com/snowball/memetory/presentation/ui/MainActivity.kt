089bc849a3a755d445d33f7544f33115e90e5ddc
youngseo3 <wys4475@gmail.com>
2024-02-07 21:33:39 +0900

    [#13] 바텀네비게이션뷰 구현

--- Android/app/src/main/java/com/snowball/memetory/presentation/ui/MainActivity.kt
+++ Android/app/src/main/java/com/snowball/memetory/presentation/ui/MainActivity.kt
@@ -2,11 +2,25 @@ package com.snowball.memetory.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.snowball.memetory.R
+import com.snowball.memetory.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigationView = binding.bottomNavigation
+        val navController = findNavController(R.id.mainNavHost)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+
+
     }
 }
