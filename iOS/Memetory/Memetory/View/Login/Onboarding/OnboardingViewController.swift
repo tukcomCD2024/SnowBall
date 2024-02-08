c6ec3f4dc60de29331c476954977cbc89b2d8e86
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2024-02-08 14:57:04 +0900

    [#7]NickNameSetView 추가

---
+++ iOS/Memetory/Memetory/View/Login/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingViewController.swift
+//  Memetory
+//
+//  Created by 이승진 on 2024/02/08.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
