307c655b3b7c7b507c0a38970c548210e77fc56e
youngseo3 <wys4475@gmail.com>
2024-02-20 00:05:58 +0900

    [#13] GenerateMemeFragment UI

--- Android/app/src/main/java/com/snowball/memetory/presentation/ui/generatememe/GenerateMemeFragment.kt
+++ Android/app/src/main/java/com/snowball/memetory/presentation/ui/generatememe/GenerateMemeFragment.kt
@@ -1,33 +1,22 @@
 package com.snowball.memetory.presentation.ui.generatememe
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.snowball.memetory.R
+import com.snowball.memetory.databinding.FragmentGenerateMemeBinding
+import com.snowball.memetory.presentation.ui.generatememe.adapter.TemplateRVAdater
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [GenerateMemeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class GenerateMemeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class GenerateMemeFragment : Fragment(), TemplateRVAdater.OnItemClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +24,25 @@ class GenerateMemeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_generate_meme, container, false)
+//        startActivity(Intent(activity, GenerateMemeActivity::class.java))
+
+        val binding: FragmentGenerateMemeBinding = DataBindingUtil.inflate(inflater,
+                                                    R.layout.fragment_generate_meme,
+                                                    container,
+                                                    false)
+        val arrayList = arrayListOf<Int>(3, 4, 5, 6)
+        binding.templateRecyclerView.adapter = TemplateRVAdater(arrayList, this)
+
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GenerateMemeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GenerateMemeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    override fun onItemClick(view: View, position: Int) {
+        val intent = Intent(activity, GenerateMemeActivity::class.java)
+        // 필요한 경우 intent에 데이터 추가
+        // intent.putExtra("extra_data", value)
+        startActivity(intent)
     }
+
 }
---
+++ Android/app/src/main/java/com/snowball/memetory/presentation/ui/generatememe/adapter/TemplateRVAdater.kt
@@ -0,0 +1,44 @@
+package com.snowball.memetory.presentation.ui.generatememe.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.snowball.memetory.R
+import com.snowball.memetory.databinding.ItemTemplateBinding
+
+class TemplateRVAdater(private val imgRes: ArrayList<Int>, private val itemClick: OnItemClickListener)
+    : RecyclerView.Adapter<TemplateRVAdater.ViewHolder>() {
+
+    interface OnItemClickListener {
+        fun onItemClick(view: View, position: Int)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TemplateRVAdater.ViewHolder {
+        val view = DataBindingUtil.inflate<ItemTemplateBinding>(LayoutInflater.from(parent.context), R.layout.item_template, parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TemplateRVAdater.ViewHolder, position: Int) {
+        holder.img.text = imgRes[position].toString()
+    }
+
+    override fun getItemCount(): Int {
+        return imgRes.size
+    }
+
+    inner class ViewHolder(binding : ItemTemplateBinding): RecyclerView.ViewHolder(binding.root) {
+        val img = binding.templateNumText
+
+        init {
+            itemView.setOnClickListener {
+                itemClick.onItemClick(it, adapterPosition)
+
+            }
+        }
+    }
+}
