719a82b9bc2efeb62c0ecb2414040b74e410e8ab
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2024-03-06 03:02:41 +0900

    [#18] StorageView 수정

--- iOS/Memetory/Memetory/View/StorageViewController.swift
+++ iOS/Memetory/Memetory/View/StorageViewController.swift
@@ -6,15 +6,37 @@
 //
 
 import UIKit
+import SnapKit
 
-class StorageViewController: UITabBarController {
+class StorageViewController: UIViewController {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "보관함"
+        label.textColor = .black
+//        label.font = .boldSystemFont(ofSize: 25)
+        label.font = UIFont(name: "Pretendard-Bold", size: 25)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        setViews()
+        setConstrainsts()
     }
     
+    private func setViews() {
+        view.addSubview(titleLabel)
+    }
+    
+    private func setConstrainsts() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(80)
+            make.leading.equalToSuperview().offset(15)
+        }
+    }
 
     /*
     // MARK: - Navigation
