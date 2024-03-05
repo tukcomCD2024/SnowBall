427d8d34b5964453591b6052f074e8aee33f9509
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2024-03-06 03:18:55 +0900

    [#18] Test Model - Scene 추가

---
+++ iOS/Memetory/Memetory/View/Meme-generate/Models/Scene.swift
@@ -0,0 +1,19 @@
+//
+//  Scene.swift
+//  Memetory
+//
+//  Created by 이승진 on 2024/03/05.
+//
+
+import UIKit
+
+struct Scene {
+    static let sceneImage = ["demo1", "demo2"]
+    
+    
+//    lazy var sceneImage: UIImage? = {
+//        return UIImage(named: "demo\(num).jpeg")
+//    }()
+//    
+//    var num: Int
+}
