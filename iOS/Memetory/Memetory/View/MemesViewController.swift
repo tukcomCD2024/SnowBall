0e6bacb72d6fb4eba615aff891ace3e6768970e4
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2024-03-06 03:03:06 +0900

    [#18] MemesView 수정

--- iOS/Memetory/Memetory/View/MemesViewController.swift
+++ iOS/Memetory/Memetory/View/MemesViewController.swift
@@ -6,24 +6,35 @@
 //
 
 import UIKit
+import SnapKit
 
-class MemesViewController: UITabBarController {
+class MemesViewController: UIViewController {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "밈's"
+        label.textColor = .black
+//        label.font = .boldSystemFont(ofSize: 25)
+        label.font = UIFont(name: "Pretendard-Bold", size: 25)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .darkGray
+        view.backgroundColor = .white
+        setViews()
+        setConstrainsts()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setViews() {
+        view.addSubview(titleLabel)
     }
-    */
-
+    
+    private func setConstrainsts() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(80)
+            make.leading.equalToSuperview().offset(15)
+        }
+    }
 }
