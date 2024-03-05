0b6d89ac7d9bc1deecdff72bf4b1f7a9ab48acea
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2024-03-06 03:03:33 +0900

    [#18] HomeView 수정

--- iOS/Memetory/Memetory/View/HomeViewController.swift
+++ iOS/Memetory/Memetory/View/HomeViewController.swift
@@ -6,13 +6,37 @@
 //
 
 import UIKit
+import SnapKit
 
-class HomeViewController: UITabBarController {
+class HomeViewController: UIViewController {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이달의 좋아요"
+        label.textColor = .black
+//        label.font = .boldSystemFont(ofSize: 25)
+        label.font = UIFont(name: "Pretendard-Bold", size: 25)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .brown
+        view.backgroundColor = .white
+        setViews()
+        setConstrainsts()
+    }
+    
+    private func setViews() {
+        view.addSubview(titleLabel)
+       
+    }
+    
+    private func setConstrainsts() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(80)
+            make.leading.equalToSuperview().offset(15)
+        }
     }
     
 
