c81d0ce03fb80353ab21ef8c0b21bd06a6776b3b
ggamD00 <jaehuek@naver.com>
2024-03-09 23:38:23 +0900

    [#29] S3 파일 다운로드 함수 추가

--- AI/roop/s3/s3.py
+++ AI/roop/s3/s3.py
@@ -50,4 +50,19 @@ class S3:
             print(e)
             return False
         return "s3 업로드 성공!"
-    
+
+    def s3_get_object(s3, bucket, object_name, file_name):
+        '''
+        s3 bucket에서 지정 파일 다운로드
+        :param s3: 연결된 s3 객체(boto3 client)
+        :param bucket: 버킷명
+        :param object_name: s3에 저장된 object 명
+        :param file_name: 저장할 파일 명(path)
+        :return: 성공 시 True, 실패 시 False 반환
+        '''
+        try:
+            s3.download_file(bucket, object_name, file_name)
+        except Exception as e:
+            print(e)
+            return False
+        return True
