eb0ac205f09da55a0a983ca1ed9fa7d43db925d7
JunRain <sa2313@tukorea.ac.kr>
2024-03-11 21:02:07 +0900

    [#33] meme 엔티티 구현

---
+++ backend/memetory/src/main/java/com/example/memetory/domain/meme/entity/Meme.java
@@ -0,0 +1,29 @@
+package com.example.memetory.domain.meme.entity;
+
+import com.example.memetory.domain.member.entity.Member;
+import com.example.memetory.global.entity.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Meme extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="member_id")
+	private Member member;
+
+	private String name;
+	private String s3Url;
+}
