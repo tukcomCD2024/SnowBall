cf42b159aee2ed708ab1d0b937de3d612f68a4c7
ggamD00 <jaehuek@naver.com>
2024-03-12 14:34:45 +0900

    [#29] 파일을 바로 저장하는 것이 아닌 객체로 받아오게 하기 위해 download_file -> download_fileobj로 변경

--- AI/roop/s3/s3_request.py
+++ AI/roop/s3/s3_request.py
@@ -1,3 +1,5 @@
+import io
+
 import boto3
 import yaml
 
@@ -35,7 +37,7 @@ def s3_put_object(s3, file_obj, access_key):
     """
     s3 bucket에 지정 파일 업로드
     :param s3: 연결된 s3 객체(boto3 client)
-    :param filepath: 파일 위치
+    :param file_obj: 파일 객체
     :param access_key: 저장 파일명
     :return: 성공 시 True, 실패 시 False 반환
     """
@@ -47,17 +49,25 @@ def s3_put_object(s3, file_obj, access_key):
     return "s3 업로드 성공!"
 
 
-def s3_get_object(s3, object_name, file_name):
+def s3_get_object(s3, object_name):
     """
-    s3 bucket에서 지정 파일 다운로드
-    :param s3: 연결된 s3 객체(boto3 client)
-    :param object_name: s3에 저장된 object 명
-    :param file_name: 저장할 파일 명(path)
-    :return: 성공 시 True, 실패 시 False 반환
+    S3 버킷에서 파일 객체를 다운로드합니다.
+
+    :param s3: 연결된 S3 클라이언트 (boto3 클라이언트)
+    :param object_name: S3 버킷에 저장된 객체의 이름
+    :return: 성공 시 다운로드된 파일의 내용을 포함하는 파일 유사 객체, 실패 시 None 반환
     """
     try:
-        s3.download_file(BUKET, object_name, file_name)
+        # 다운로드된 파일을 저장할 메모리 내 파일 유사 객체를 생성합니다.
+        source_image = io.BytesIO()
+
+        # S3 버킷에서 파일을 파일 유사 객체로 다운로드합니다.
+        s3.download_fileobj(BUKET, object_name, source_image)
+
+        # 파일 유사 객체의 포인터를 파일의 처음으로 되돌립니다.
+        source_image.seek(0)
+
+        return source_image
     except Exception as e:
         print(e)
-        return False
-    return True
+        return None
