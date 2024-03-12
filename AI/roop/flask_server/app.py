7f6b75103ef8268ee807093f05f5566580bd72a4
ggamD00 <jaehuek@naver.com>
2024-03-12 14:33:35 +0900

    [#29] 요청값을 base64 인코딩 값에서 s3_url로 바꿈

--- AI/roop/flask_server/app.py
+++ AI/roop/flask_server/app.py
@@ -1,18 +1,19 @@
 import base64
-import os
 import subprocess
 from io import BytesIO
+from urllib.parse import urlparse
 
 from PIL import Image
 from flask import Flask, request, jsonify
-from werkzeug.utils import secure_filename
 from d_id.did_reqeust import DIdAPI
 import hashlib
 import time
 
 from shotstack.shot_stack import ShotStackAPI
+from s3 import s3_request
 
 app = Flask(__name__)
+s3 = s3_request.s3_connection()
 
 
 # 파일 업로드 처리
@@ -45,19 +46,25 @@ def process_data():
         for item in data:
             text_data = item.get('text')
             target_image_number = item.get('target_image')
-            source_image_base64 = item.get('source_image')
+            source_image_s3_url = item.get('source_image')
+            file_name = get_file_name_from_url(source_image_s3_url)
 
             # Base64 디코딩 및 처리 예시 (이미지를 파일로 저장하는 경우)
-            if source_image_base64:
-                image_data = base64.b64decode(source_image_base64)
-                image = Image.open(BytesIO(image_data))
+            if file_name:
+                source_image = s3_request.s3_get_object(s3, file_name)
 
                 # 현재 시간을 기반으로 한 해시값 생성
                 hash_input = str(time.time())
                 unique_hash = hashlib.sha256(hash_input.encode()).hexdigest()[:4]
 
-                # 고유한 파일 이름으로 저장 (예: decoded_image_<unique_hash>.jpg)
-                image.save(f'../image/{unique_hash}.jpg')
+                # 파일을 저장할 경로 및 파일 이름 생성
+                file_path = f"../image/{unique_hash}.jpg"
+
+                # BytesIO 객체에 저장된 데이터를 파일로 저장
+                if save_bytes_io_to_file(source_image, file_path):
+                    print(f"파일 저장 성공: {file_path}")
+                else:
+                    print(f"파일 저장 실패: {file_path}")
 
             face_swap_image_name = face_swap(target_image_number, unique_hash)
             talk_id = did.run(face_swap_image_name, text_data)
@@ -125,6 +132,40 @@ def face_swap(target_image_number, source_image):
     face_swap_image_name = source_image + "-" + target_image_number + '.jpg'
 
     return face_swap_image_name
+
+
+def save_bytes_io_to_file(bytes_io, file_path):
+    """
+    BytesIO 객체에 저장된 데이터를 파일로 저장합니다.
+
+    :param bytes_io: BytesIO 객체
+    :param file_path: 저장할 파일 경로
+    :return: 저장에 성공하면 True, 실패하면 False를 반환합니다.
+    """
+    try:
+        with open(file_path, "wb") as file:
+            file.write(bytes_io.getvalue())
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+def get_file_name_from_url(url):
+    """
+    URL에서 파일 이름을 추출합니다.
+
+    :param url: 파일이 위치한 URL
+    :return: 추출된 파일 이름 또는 None (URL에서 파일 이름을 추출할 수 없는 경우)
+    """
+    try:
+        parsed_url = urlparse(url)
+        # URL 경로를 '/'로 분할하여 파일 이름을 추출
+        file_name = parsed_url.path.split('/')[-1]
+        return file_name
+    except Exception as e:
+        print(e)
+        return None
 
 
 if __name__ == '__main__':
