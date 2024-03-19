7531cb3a98d2712d7a01ebd01db161c667b9c325
JunRain <sa2313@tukorea.ac.kr>
2024-03-19 18:19:59 +0900

    [#40] register의 매개변수를 memeServiceDto로 변경

--- backend/memetory/src/main/java/com/example/memetory/domain/meme/dto/MemeServiceDto.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/meme/dto/MemeServiceDto.java
@@ -1,5 +1,8 @@
 package com.example.memetory.domain.meme.dto;
 
+import com.example.memetory.domain.member.entity.Member;
+import com.example.memetory.domain.meme.entity.Meme;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,4 +13,11 @@ import lombok.Getter;
 public class MemeServiceDto {
 	private Long memberId;
 	private String s3Url;
+
+	public Meme toEntity(Member member) {
+		return Meme.builder()
+			.member(member)
+			.s3Url(this.s3Url)
+			.build();
+	}
 }
--- backend/memetory/src/main/java/com/example/memetory/domain/meme/service/MemeService.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/meme/service/MemeService.java
@@ -5,6 +5,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.example.memetory.domain.member.entity.Member;
 import com.example.memetory.domain.member.service.MemberService;
+import com.example.memetory.domain.meme.dto.MemeServiceDto;
 import com.example.memetory.domain.meme.dto.ShotStackCallBackRequest;
 import com.example.memetory.domain.meme.entity.Meme;
 import com.example.memetory.domain.meme.repository.MemeRepository;
@@ -18,9 +19,9 @@ public class MemeService {
 	private final MemeRepository memeRepository;
 
 	@Transactional
-	public void register(Long id, ShotStackCallBackRequest shotStackCallBackRequest) {
-		Member member = memberService.findById(id);
-		Meme meme = shotStackCallBackRequest.toEntity(member);
+	public void register(MemeServiceDto memeServiceDto) {
+		Member member = memberService.findById(memeServiceDto.getMemberId());
+		Meme meme = memeServiceDto.toEntity(member);
 
 		memeRepository.save(meme);
 	}
