3fe3d471c0f85e22a53710a8e6686f490346093c
JunRain <sa2313@tukorea.ac.kr>
2024-03-21 12:06:50 +0900

    [#42] @SerializedName을 통해서 json 형식을 맞춤

--- backend/memetory/src/main/java/com/example/memetory/domain/meme/dto/AIServerSendDto.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/meme/dto/AIServerSendDto.java
@@ -2,6 +2,8 @@ package com.example.memetory.domain.meme.dto;
 
 import java.util.List;
 
+import com.google.gson.annotations.SerializedName;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,7 +12,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AIServerSendDto {
+	@SerializedName("member_id")
 	private String memberId;
+	@SerializedName("scene")
 	private List<GenerateMemeDto> scene;
 
 	@Builder
--- backend/memetory/src/main/java/com/example/memetory/domain/meme/dto/GenerateMemeDto.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/meme/dto/GenerateMemeDto.java
@@ -1,5 +1,7 @@
 package com.example.memetory.domain.meme.dto;
 
+import com.google.gson.annotations.SerializedName;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,7 +9,10 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class GenerateMemeDto {
+	@SerializedName("source_image")
 	private String sourceImage;
+	@SerializedName("target_image")
 	private String targetImage;
+	@SerializedName("name")
 	private String text;
 }
