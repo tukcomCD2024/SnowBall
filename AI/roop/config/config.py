8b271a6bc6b223d6445f3dabfb86a307c28f3bc9
JunRain <sa2313@tukorea.ac.kr>
2024-03-26 11:59:36 +0900

    [#46] .env파일의 데이터를 불러오는 Config 클래스 구현

---
+++ AI/roop/config/config.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+class Config:
+    _instance = None
+
+    DEBUG = False
+    DID_API_KEY = None
+    ELEVENLABS_API_KEY = None
+    SHOT_STACK_API_KEY = None
+    AWS_ACCESS_KEY_ID = None
+    AWS_SECRET_ACCESS_KEY = None
+    AWS_S3_BUCKET_REGION = None
+    AWS_S3_BUCKET_NAME = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            load_dotenv()
+            cls._instance.DID_API_KEY = os.environ.get("D-ID_API_KEY")
+            cls._instance.ELEVENLABS_API_KEY = os.environ.get("ELEVENLABS_API_KEY")
+            cls._instance.SHOT_STACK_API_KEY = os.environ.get("SHOT_STACK_API_KEY")
+            cls._instance.AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+            cls._instance.AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+            cls._instance.AWS_S3_BUCKET_REGION = os.environ.get("AWS_S3_BUCKET_REGION")
+            cls._instance.AWS_S3_BUCKET_NAME = os.environ.get("AWS_S3_BUCKET_NAME")
+        return cls._instance
+
+# 사용 예시:
+# config = Config()
+# print(config.ID_API_KEY)
