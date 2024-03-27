6a6682649ff5fd59d74dd8f1ebbb2bc55ca5592b
youngseo3 <wys4475@gmail.com>
2024-03-27 20:41:18 +0900

    [#25] ChooseTemplateFragment
    사진 변경 및 뷰모델 사용하여 코드 변경

--- Android/app/src/main/java/com/snowball/memetory/presentation/ui/generatememe/ChooseTemplateFragment.kt
+++ Android/app/src/main/java/com/snowball/memetory/presentation/ui/generatememe/ChooseTemplateFragment.kt
@@ -7,10 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.viewpager2.widget.ViewPager2
 import com.snowball.memetory.R
+import com.snowball.memetory.data.dto.generatememe.SceneDetailDto
 import com.snowball.memetory.databinding.FragmentChooseTemplateBinding
 import com.snowball.memetory.presentation.ui.generatememe.adapter.TemplateDetailVPAdater
 import com.snowball.memetory.presentation.ui.generatememe.adapter.TemplateRVAdater
@@ -20,6 +23,7 @@ class ChooseTemplateFragment : Fragment() {
 
     lateinit var navController: NavController
     lateinit var binding: FragmentChooseTemplateBinding
+    private val viewModel: SceneDetailViewModel by activityViewModels()  // ViewModel 인스턴스 생성
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,25 +50,45 @@ class ChooseTemplateFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+//        var imgRes = arrayListOf<Int>(
+//            R.drawable.gamst,
+//            R.drawable.ic_play_circle_32,
+//            R.drawable.gamst,
+//            R.drawable.ic_play_circle_32
+//        )
+
         var imgRes = arrayListOf<Int>(
-            R.drawable.gamst,
-            R.drawable.ic_play_circle_32,
-            R.drawable.gamst,
-            R.drawable.ic_play_circle_32
+            R.drawable.snake,
+            R.drawable.malfoy
         )
+
         var templtVPAdapter = TemplateDetailVPAdater(imgRes)
         binding.templateViewPager.adapter = templtVPAdapter
         setOffsetBetweenPages()
 
         templtVPAdapter.itemClickListener = object : TemplateDetailVPAdater.OnItemClickListener {
             override fun onItemClick(view: View, position: Int) {
+                val resourceId = imgRes[position]
+                Log.d("ChooseTemplate", "$position+$resourceId")
+                val isSelected = viewModel.isImageSelected(resourceId)
+                viewModel.selectedTemplateId = position
+                viewModel.setImageSelected(resourceId, !isSelected)
+                Log.d("ChooseTemplate", "${viewModel.selectedTemplateId}")
+
+                // 현재 선택된 템플릿의 정보를 ViewModel에 저장
+//                val currentData = SceneDetailDto(viewModel.selectedImageUri, viewModel.enteredText)
+//                viewModel.setTemplateData(resourceId, currentData)
+
                 navController.navigate(R.id.action_chooseTemplateFragment_to_sceneDetailFragment)
+
             }
         }
 
         binding.templateViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() { // 페이지 바뀔때마다
             override fun onPageSelected(position: Int) { // 이 페이지의 번호가 선택되면
                 super.onPageSelected(position)
+//                // 현재 페이지 번호를 ViewModel에 저장
+//                viewModel.selectedTemplateId = position
                 Log.d("ViewPagerFragment", "Page ${position+1}")
             }
         })
