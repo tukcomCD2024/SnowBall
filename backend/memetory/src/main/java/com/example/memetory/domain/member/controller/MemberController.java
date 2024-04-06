801b2087a9e9202b13602673a4b76aaaa9a04a9b
JunRain <sa2313@tukorea.ac.kr>
2024-04-06 16:38:27 +0900

    [#59] MemberService에 집중되어 있는 로그인 책임 분산

--- backend/memetory/src/main/java/com/example/memetory/domain/member/controller/MemberController.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/member/controller/MemberController.java
@@ -10,23 +10,32 @@ import com.example.memetory.domain.member.dto.MemberServiceDto;
 import com.example.memetory.domain.member.dto.MemberSignUpRequest;
 import com.example.memetory.domain.member.service.MemberService;
 import com.example.memetory.global.annotation.LoginMemberEmail;
+import com.example.memetory.global.security.jwt.refresh.service.RefreshTokenService;
+import com.example.memetory.global.security.jwt.service.JwtService;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController implements MemberApi {
+	private final MemberService memberService;
+	private final JwtService jwtService;
+	private final RefreshTokenService refreshTokenService;
 
-    private final MemberService memberService;
+	@PostMapping("/sign-up")
+	@Override
+	public ResponseEntity<HttpStatus> register(
+		HttpServletResponse response, @RequestBody MemberSignUpRequest memberSignUpRequest,
+		@LoginMemberEmail String email) {
+		MemberServiceDto memberServiceDto = memberSignUpRequest.toServiceDto(email);
 
-    @PostMapping("/sign-up")
-    @Override
-    public ResponseEntity<HttpStatus> register(@RequestBody MemberSignUpRequest memberSignUpRequest,
-                                               @LoginMemberEmail String email) {
-        MemberServiceDto memberServiceDto = memberSignUpRequest.toServiceDto(email);
+		memberService.register(memberServiceDto);
 
-        memberService.register(memberServiceDto);
+		String refreshToken = jwtService.createRefreshToken();
+		jwtService.setRefreshTokenHeader(response, refreshToken);
+		refreshTokenService.updateToken(email, refreshToken);
 
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
 }
