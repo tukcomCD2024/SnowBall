83695a5e4c9e2cbb41dc5d24c3f14a0a27880274
JunRain <sa2313@tukorea.ac.kr>
2024-04-10 01:08:00 +0900

    [#62] MemberService 닉네임 중복 여부 확인 및 Member 업데이트 test 구현

--- backend/memetory/src/test/java/com/example/memetory/domain/member/MemberFixture.java
+++ backend/memetory/src/test/java/com/example/memetory/domain/member/MemberFixture.java
@@ -13,19 +13,12 @@ public class MemberFixture {
 		.email("junrain@ourservice.com")
 		.role(Role.USER)
 		.imageUrl("imageUrl")
-		.nickname("이준우")
+		.name("이준우")
+		.nickname("junRain")
 		.socialType(SocialType.GOOGLE)
 		.socialId("-1")
 		.build();
 
-	public final static Member GUEST_MEMBER = Member.builder()
-		.email("junrain@ourservice.com")
-		.role(Role.GUEST)
-		.imageUrl("imageUrl")
-		.nickname("이준우")
-		.socialType(SocialType.GOOGLE)
-		.socialId("-1")
-		.build();
 
 	public final static MemberServiceDto MEMBER_SERVICE_DTO = MemberServiceDto.builder()
 		.imageUrl("imageUrl")
--- backend/memetory/src/test/java/com/example/memetory/domain/member/service/MemberServiceTest.java
+++ backend/memetory/src/test/java/com/example/memetory/domain/member/service/MemberServiceTest.java
@@ -2,8 +2,7 @@ package com.example.memetory.domain.member.service;
 
 import static com.example.memetory.domain.member.MemberFixture.*;
 import static org.assertj.core.api.Assertions.*;
-
-import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,9 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.memetory.domain.member.entity.Member;
-import com.example.memetory.domain.member.entity.Role;
 import com.example.memetory.domain.member.repository.MemberRepository;
-import com.example.memetory.global.security.jwt.service.JwtService;
 
 @DisplayName("memberService의 ")
 @Transactional
@@ -23,24 +20,7 @@ public class MemberServiceTest {
 	@Autowired
 	private MemberService memberService;
 	@Autowired
-	private JwtService jwtService;
-	@Autowired
 	private MemberRepository memberRepository;
-
-	@Test
-	@DisplayName("회원가입이 잘 진행 되는가?")
-	void 회원가입() {
-		// given 멤버 저장
-		Member savedMember = memberRepository.save(GUEST_MEMBER);
-
-		// when 실행
-		memberService.register(MEMBER_SERVICE_DTO);
-
-		// then 확인 ROLE과 닉네임이 잘 변경 됐는지 확인
-		Optional<Member> updatedMember = memberRepository.findByEmail(GUEST_MEMBER.getEmail());
-		assertThat(MEMBER_SERVICE_DTO.getNickname()).isEqualTo(updatedMember.get().getNickname());
-		assertThat(Role.USER).isEqualTo(updatedMember.get().getRole());
-	}
 
 	@Test
 	@DisplayName("이메일을 통해서 멤버 가져오기")
@@ -67,5 +47,31 @@ public class MemberServiceTest {
 
 		// then
 		assertThat(findMember.getId()).isEqualTo(savedMember.getId());
+	}
+
+	@Test
+	@DisplayName("Member의 nickname 중복 여부 검사")
+	void nickname_중복검사() {
+		// given 멤버 저장
+		Member savedMember = memberRepository.save(MEMBER);
+
+		// then 실행
+		assertTrue(memberService.isDuplicatedNickname(MEMBER_SERVICE_DTO));
+	}
+
+	@Test
+	@DisplayName("Member의 업데이트가 잘 이루어지는가")
+	void member_업데이트() {
+		// given 멤버 저장
+		Member savedMember = memberRepository.save(MEMBER);
+
+		// when 멤버 업데이트
+		memberService.update(UPDATE_MEMBER_SERVICE_DTO);
+
+		Member findMember = memberService.findByEmail(UPDATE_MEMBER_SERVICE_DTO.getEmail());
+		// then 업데이트 됐는지 확인
+
+		assertThat(findMember.getNickname()).isEqualTo(UPDATE_MEMBER_SERVICE_DTO.getNickname());
+		assertThat(findMember.getImageUrl()).isEqualTo(UPDATE_MEMBER_SERVICE_DTO.getImageUrl());
 	}
 }
