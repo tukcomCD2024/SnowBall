a5ece19888a1b0d6404aafea4f9f15d437783031
JunRain <sa2313@tukorea.ac.kr>
2024-04-10 01:06:08 +0900

    [#62] MemberController에서 Member update 구현

--- backend/memetory/src/main/java/com/example/memetory/domain/member/controller/MemberController.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/member/controller/MemberController.java
@@ -1,10 +1,35 @@
 package com.example.memetory.domain.member.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.memetory.domain.member.dto.MemberServiceDto;
+import com.example.memetory.domain.member.dto.MemberUpdateDto;
+import com.example.memetory.domain.member.service.MemberService;
+import com.example.memetory.global.annotation.LoginMemberEmail;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController implements MemberApi {
+	private final MemberService memberService;
+
+	@PostMapping
+	ResponseEntity<HttpStatus> updateMember(@LoginMemberEmail String email,
+		@RequestBody MemberUpdateDto memberUpdateDto) {
+		MemberServiceDto memberServiceDto = memberUpdateDto.toServiceDto(email);
+
+		if (memberService.isDuplicatedNickname(memberServiceDto)) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
+		memberService.update(memberServiceDto);
+
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
 }
