fd7b5334342049162055099b9259ff0f50dd0509
JunRain <sa2313@tukorea.ac.kr>
2024-04-09 23:54:32 +0900

    [#62] 회원가입 로직 삭제로 인한 메서드 제거

--- backend/memetory/src/main/java/com/example/memetory/domain/member/controller/MemberApi.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/member/controller/MemberApi.java
@@ -1,35 +1,8 @@
 package com.example.memetory.domain.member.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-import com.example.memetory.domain.member.dto.MemberSignUpRequest;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Tag(name = "Member")
 public interface MemberApi {
 
-	@Operation(
-		summary = "회원가입",
-		description = "첫 소셜로그인 후 추가 정보 기입",
-		security = {@SecurityRequirement(name = "access_token")}
-	)
-	@ApiResponses(value = {
-		@ApiResponse(
-			responseCode = "200",
-			description = "회원가입 성공!"
-		)
-	})
-	ResponseEntity<HttpStatus> register(
-		HttpServletResponse response,
-		MemberSignUpRequest memberSignUpRequest,
-		@Parameter(hidden = true) String email
-	);
 }
--- backend/memetory/src/main/java/com/example/memetory/domain/member/controller/MemberController.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/member/controller/MemberController.java
@@ -1,41 +1,10 @@
 package com.example.memetory.domain.member.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.memetory.domain.member.dto.MemberServiceDto;
-import com.example.memetory.domain.member.dto.MemberSignUpRequest;
-import com.example.memetory.domain.member.service.MemberService;
-import com.example.memetory.global.annotation.LoginMemberEmail;
-import com.example.memetory.global.security.jwt.refresh.service.RefreshTokenService;
-import com.example.memetory.global.security.jwt.service.JwtService;
-
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController implements MemberApi {
-	private final MemberService memberService;
-	private final JwtService jwtService;
-	private final RefreshTokenService refreshTokenService;
-
-	@PostMapping("/sign-up")
-	@Override
-	public ResponseEntity<HttpStatus> register(
-		HttpServletResponse response, @RequestBody MemberSignUpRequest memberSignUpRequest,
-		@LoginMemberEmail String email) {
-		MemberServiceDto memberServiceDto = memberSignUpRequest.toServiceDto(email);
-
-		memberService.register(memberServiceDto);
-
-		String refreshToken = jwtService.createRefreshToken();
-		jwtService.setRefreshTokenHeader(response, refreshToken);
-		refreshTokenService.updateToken(email, refreshToken);
-
-		return ResponseEntity.status(HttpStatus.OK).build();
-	}
 }
