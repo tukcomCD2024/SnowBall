bd290f877d2d889c2b2d91e6cfafaf79293f4ced
JunRain <sa2313@tukorea.ac.kr>
2024-04-09 23:47:31 +0900

    [#62] SocialType에 필드값으로 공급자 Url 추가 및 kakao 삭제

--- backend/memetory/src/main/java/com/example/memetory/domain/member/entity/SocialType.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/member/entity/SocialType.java
@@ -1,5 +1,19 @@
 package com.example.memetory.domain.member.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum SocialType {
-	KAKAO, GOOGLE
+	GOOGLE("https://www.googleapis.com/oauth2/v2/userinfo");
+
+	private final String providerUrl;
+
+	@JsonCreator
+	public static SocialType from(String s) {
+		return SocialType.valueOf(s.toUpperCase());
+	}
 }
