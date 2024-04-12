007a9fa8819a8a2ffb38792898f4280d346b22a7
ggamD00 <jaehuek@naver.com>
2024-04-13 01:29:46 +0900

    [#50] 댓글 삭제 requestDto 구현

--- backend/memetory/src/main/java/com/example/memetory/domain/comment/dto/request/DeleteCommentRequest.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/comment/dto/request/DeleteCommentRequest.java
@@ -1,2 +1,19 @@
-package com.example.memetory.domain.comment.dto.request;public class DeleteCommentRequest {
+package com.example.memetory.domain.comment.dto.request;
+
+import com.example.memetory.domain.comment.dto.CommentServiceDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class DeleteCommentRequest {
+
+    private Long memesId;
+
+    public CommentServiceDto toServiceDto(Long commentId) {
+        return CommentServiceDto.builder()
+                .memesId(memesId)
+                .commentId(commentId)
+                .build();
+    }
 }
