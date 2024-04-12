371a5928be9042d052cc944a67c70656188733ac
ggamD00 <jaehuek@naver.com>
2024-04-13 01:32:54 +0900

    [#50] url 리소스 /comment -> /comments 변경

--- backend/memetory/src/main/java/com/example/memetory/domain/comment/controller/CommentController.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/comment/controller/CommentController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/comment")
+@RequestMapping("/comments")
 @RequiredArgsConstructor
 public class CommentController implements CommentApi{
 
