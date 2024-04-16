143fa7a62b8697e952b5bc37dd3af6a208d87d82
ggamD00 <jaehuek@naver.com>
2024-04-17 00:39:13 +0900

    [#68] 신고 ServiceDto 및 신고 생성 requestDto 구현

---
+++ backend/memetory/src/main/java/com/example/memetory/domain/complain/dto/ComplainServiceDto.java
@@ -0,0 +1,25 @@
+package com.example.memetory.domain.complain.dto;
+
+import com.example.memetory.domain.complain.entity.Complain;
+import com.example.memetory.domain.member.entity.Member;
+import com.example.memetory.domain.memes.entity.Memes;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ComplainServiceDto {
+    private String email;
+    private String content;
+    private Long memesId;
+
+    public Complain toEntity(Member member, Memes memes) {
+        return Complain.builder()
+                .content(content)
+                .member(member)
+                .memes(memes)
+                .build();
+    }
+}
