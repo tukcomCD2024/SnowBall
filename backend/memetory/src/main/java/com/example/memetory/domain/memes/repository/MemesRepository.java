c7151811592e6727c57c387316bb68785a93df40
ggamD00 <jaehuek@naver.com>
2024-04-18 03:07:09 +0900

    [#73] 좋아요 수 기준 상위 10개의 밈스를 조회하는 메서드
    like_count 기준으로 내림차순 정렬 후 10개를 가져온다. 이 때 limit은 jpql에 없기 때문에 nativeQuery를 true로 해준다.

--- backend/memetory/src/main/java/com/example/memetory/domain/memes/repository/MemesRepository.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/memes/repository/MemesRepository.java
@@ -2,6 +2,12 @@ package com.example.memetory.domain.memes.repository;
 
 import com.example.memetory.domain.memes.entity.Memes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MemesRepository extends JpaRepository<Memes, Long> {
+
+    @Query(value = "select * from memes ORDER BY like_count desc limit 10", nativeQuery = true)
+    List<Memes> findTopTenMemesByLikeCount();
 }
