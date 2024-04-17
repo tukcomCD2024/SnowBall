ff0e2c36d1d7e7ca7a19a52a9032ad79466a9beb
JunRain <sa2313@tukorea.ac.kr>
2024-04-17 19:52:51 +0900

    [#59] MemeService.getAllMeme() 테스트

--- backend/memetory/src/test/java/com/example/memetory/domain/meme/service/MemeServiceTest.java
+++ backend/memetory/src/test/java/com/example/memetory/domain/meme/service/MemeServiceTest.java
@@ -5,17 +5,19 @@ import static com.example.memetory.domain.meme.MemeFixture.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.example.memetory.domain.member.entity.Member;
 import com.example.memetory.domain.member.repository.MemberRepository;
 import com.example.memetory.domain.member.service.MemberService;
+import com.example.memetory.domain.meme.dto.MemeListResponse;
 import com.example.memetory.domain.meme.dto.MemeResponse;
 import com.example.memetory.domain.meme.dto.MemeServiceDto;
 import com.example.memetory.domain.meme.entity.Meme;
@@ -78,5 +80,27 @@ public class MemeServiceTest {
 		// given
 		assertThat(memeResponse.getMemeId()).isEqualTo(expectedResponse.getMemeId());
 		assertThat(memeResponse.getS3Url()).isEqualTo(expectedResponse.getS3Url());
+	}
+
+	@Test
+	@DisplayName("Member를 통한 해당 멤버의 전체 밈 조회")
+	void 전체_밈_조회() {
+		// given 밈들 저장, MemeListResponse 세팅
+		given(memberService.findById(savedMember.getId())).willReturn(savedMember);
+		MemeServiceDto memeServiceDto = MemeServiceDto.builder()
+			.email(savedMember.getEmail())
+			.build();
+
+		List<Meme> memeList = List.of(memeRepository.save(FIRST_MEME(savedMember)),
+			memeRepository.save(SECOND_MEME(savedMember)));
+		MemeListResponse expectedResponse = MemeListResponse.builder()
+			.memeList(memeList.stream().map(MemeResponse::of).toList())
+			.build();
+
+		// when
+		MemeListResponse response = memeService.getAllMeme(memeServiceDto);
+
+		// then
+		assertThat(response).usingRecursiveComparison().isEqualTo(expectedResponse);
 	}
 }
