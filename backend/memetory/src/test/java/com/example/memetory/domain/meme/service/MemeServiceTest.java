f0b4022f02a5496043f9eb6c8ce08581afe964ea
JunRain <sa2313@tukorea.ac.kr>
2024-04-17 19:02:08 +0900

    [#59] MemeService.register() 테스트

--- backend/memetory/src/test/java/com/example/memetory/domain/meme/service/MemeServiceTest.java
+++ backend/memetory/src/test/java/com/example/memetory/domain/meme/service/MemeServiceTest.java
@@ -1,7 +1,23 @@
 package com.example.memetory.domain.meme.service;
 
+import static com.example.memetory.domain.member.MemberFixture.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.example.memetory.domain.member.entity.Member;
+import com.example.memetory.domain.member.repository.MemberRepository;
+import com.example.memetory.domain.member.service.MemberService;
+import com.example.memetory.domain.meme.dto.MemeServiceDto;
+import com.example.memetory.domain.meme.entity.Meme;
+import com.example.memetory.domain.meme.repository.MemeRepository;
 
 import jakarta.transaction.Transactional;
 
@@ -9,4 +25,38 @@ import jakarta.transaction.Transactional;
 @Transactional
 @SpringBootTest
 public class MemeServiceTest {
+	@Autowired
+	private MemeRepository memeRepository;
+	@Autowired
+	private MemeService memeService;
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@Mock
+	private MemberService memberService;
+
+	private Member savedMember;
+
+	@BeforeEach
+	void setUp() {
+		savedMember = memberRepository.save(MEMBER());
+	}
+
+	@Test
+	@DisplayName("밈 저장 성공")
+	void 밈_저장() {
+		// given Meme을 세팅
+		given(memberService.findById(savedMember.getId())).willReturn(savedMember);
+
+		MemeServiceDto memeServiceDto = MemeServiceDto.builder()
+			.memberId(savedMember.getId())
+			.s3Url("myS3Url")
+			.build();
+
+		// when
+		memeService.register(memeServiceDto);
+
+		// then
+		assertThat(memeRepository.findAllByMember(savedMember).get(0).getMember()).isEqualTo(savedMember);
+	}
 }
