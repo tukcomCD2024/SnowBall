fa57eaf092acfe0c17f822aa371f96ae1c71289a
ggamD00 <jaehuek@naver.com>
2024-04-19 03:19:22 +0900

    [#73] MemesRepository 이달의 인기차트 조회 구현
    최근 한 달 동안 생성된 밈 중을 대상으로 like_count 기준 내림차순 정렬 후 10개를 가져온다.

--- backend/memetory/src/main/java/com/example/memetory/domain/memes/repository/MemesRepository.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/memes/repository/MemesRepository.java
@@ -10,4 +10,7 @@ public interface MemesRepository extends JpaRepository<Memes, Long> {
 
     @Query(value = "select * from memes ORDER BY like_count desc limit 10", nativeQuery = true)
     List<Memes> findTopTenMemesByLikeCount();
+
+    @Query(value = "SELECT * FROM memes WHERE created_at >= DATE_SUB(NOW(), INTERVAL 1 MONTH) ORDER BY like_count DESC LIMIT 10", nativeQuery = true)
+    List<Memes> findTopTenMemesByLikeCountForMonth();
 }
