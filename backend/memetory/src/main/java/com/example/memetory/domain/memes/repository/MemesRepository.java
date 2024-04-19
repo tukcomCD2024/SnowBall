8da00e071859c2a34404bd0d7446c221f1e2df19
ggamD00 <jaehuek@naver.com>
2024-04-20 02:21:35 +0900

    [#73] month, week로 나눠져있던 메서드를 period 메서드 하나로 통합

--- backend/memetory/src/main/java/com/example/memetory/domain/memes/repository/MemesRepository.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/memes/repository/MemesRepository.java
@@ -13,13 +13,9 @@ public interface MemesRepository extends JpaRepository<Memes, Long> {
 
     @Query("SELECT m FROM Memes m ORDER BY m.likeCount DESC")
     @EntityGraph(attributePaths = {"meme"})
-    List<Memes> findTopTenMemesByLikeCount(Pageable pageable);
+    List<Memes> findTopMemesByLikeCount(Pageable pageable);
 
     @Query("SELECT m FROM Memes m WHERE m.createdAt >= :oneMonthAgo ORDER BY m.likeCount DESC")
     @EntityGraph(attributePaths = {"meme"})
-    List<Memes> findTopTenMemesByLikeCountForMonth(Pageable pageable, LocalDateTime oneMonthAgo);
-
-    @Query("SELECT m FROM Memes m WHERE m.createdAt >= :oneWeekAgo ORDER BY m.likeCount DESC")
-    @EntityGraph(attributePaths = {"meme"})
-    List<Memes> findTopTenMemesByLikeCountForWeek(Pageable pageable, LocalDateTime oneWeekAgo);
+    List<Memes> findTopMemesByLikeCountForPeriod(Pageable pageable, LocalDateTime oneMonthAgo);
 }
