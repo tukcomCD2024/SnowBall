8b0484451c6a55a5c0fcb20bac0f496a41684487
JunRain <sa2313@tukorea.ac.kr>
2024-04-19 17:40:35 +0900

    [#59] 단일 밈 조회 테스트 구현

--- backend/memetory/src/test/java/com/example/memetory/domain/meme/controller/MemeControllerTest.java
+++ backend/memetory/src/test/java/com/example/memetory/domain/meme/controller/MemeControllerTest.java
@@ -1,2 +1,55 @@
-package com.example.memetory.domain.meme.controller;public class MemeCotrollerTest {
+package com.example.memetory.domain.meme.controller;
+
+import static com.example.memetory.domain.meme.MemeFixture.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.example.memetory.domain.meme.dto.MemeListResponse;
+import com.example.memetory.domain.meme.dto.MemeResponse;
+import com.example.memetory.domain.meme.entity.Meme;
+import com.example.memetory.domain.meme.exception.NotFoundMemeException;
+import com.example.memetory.domain.meme.service.MemeService;
+import com.example.memetory.global.LoginTest;
+
+@DisplayName("Meme 컨트롤러 테스트의 ")
+@WebMvcTest(MemeController.class)
+public class MemeControllerTest extends LoginTest {
+	@MockBean
+	private MemeService memeService;
+
+	/**
+	 *Todo
+	 * register와 callback은 외부 API를 활용하는 메서드이기 떄문에 테스트 법 공부가 필요
+	 */
+
+	@Test
+	@DisplayName("단일 밈 조회 성공")
+	void 단일_밈_조회_성공() throws Exception {
+		// given 예상될 MemeResponse 구현
+		Meme meme = FIRST_MEME(loginMember);
+		given(memeService.checkMember(any())).willReturn(false);
+		given(memeService.getMeme(any())).willReturn(MemeResponse.of(meme));
+
+		// when
+		final ResultActions perform = mockMvc.perform(
+			get("/meme/-1")
+				.contentType(MediaType.APPLICATION_JSON)
+				.header("Authorization", "Bearer " + accessToken)
+		).andDo(print());
+
+		// then
+		perform.andExpect(status().isOk())
+			.andExpect(jsonPath("$.s3Url").exists());
+	}
 }
