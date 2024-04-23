921b4efef2f4eabc25c6fe520c50e2a0ddf9ea9b
ggamD00 <jaehuek@naver.com>
2024-04-24 01:13:08 +0900

    [#73] @EntityGraph을 통한 left join을 위해 findById()을 쓰지 않고 새로 구현

--- backend/memetory/src/main/java/com/example/memetory/domain/memes/repository/MemesRepository.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/memes/repository/MemesRepository.java
@@ -4,13 +4,17 @@ import com.example.memetory.domain.memes.entity.Memes;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface MemesRepository extends JpaRepository<Memes, Long> {
+
+    @Query("SELECT ms FROM Memes ms WHERE ms.Id = :memesId")
+    @EntityGraph(attributePaths = {"meme", "member"})
+    Optional<Memes> findByMemesId(Long memesId);
 
     @Query("SELECT ms FROM Memes ms ORDER BY ms.likeCount DESC")
     @EntityGraph(attributePaths = {"meme"})
