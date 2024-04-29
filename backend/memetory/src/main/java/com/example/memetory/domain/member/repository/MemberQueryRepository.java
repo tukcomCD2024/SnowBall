3389013df896bfa5ffe971a010da1cee9af3118d
JunRain <sa2313@tukorea.ac.kr>
2024-04-30 01:54:41 +0900

    [#59] Member 엔티티에 대한 조회 메서드들을 모아둔 MemberQueryRepository 구현

---
+++ backend/memetory/src/main/java/com/example/memetory/domain/member/repository/MemberQueryRepository.java
@@ -0,0 +1,14 @@
+package com.example.memetory.domain.member.repository;
+
+import java.util.Optional;
+
+import com.example.memetory.domain.member.entity.Member;
+import com.example.memetory.domain.member.entity.SocialType;
+
+public interface MemberQueryRepository {
+	Optional<Member> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
+
+	Optional<Member> findByEmail(String email);
+
+	boolean existsMemberByNickname(String email);
+}
