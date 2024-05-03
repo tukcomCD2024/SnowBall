4125365395e6e7449133d6cd4661f69e9aa2e050
JunRain <sa2313@tukorea.ac.kr>
2024-05-04 02:24:04 +0900

    [#78] BuisnessException 구현

---
+++ backend/memetory/src/main/java/com/example/memetory/global/exception/BusinessException.java
@@ -0,0 +1,15 @@
+package com.example.memetory.global.exception;
+
+import com.example.memetory.global.response.ErrorCode;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+	private final ErrorCode errorCode;
+
+	public BusinessException(ErrorCode errorCode) {
+		super(errorCode.getMessage());
+		this.errorCode = errorCode;
+	}
+}
