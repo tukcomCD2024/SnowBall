0d596e985cfd005d07567c36d2bb67e99bb54375
JunRain <sa2313@tukorea.ac.kr>
2024-05-04 02:50:54 +0900

    [#78] ExceptionAdvice 리팩토링

--- backend/memetory/src/main/java/com/example/memetory/global/exception/advice/ExceptionAdvice.java
+++ backend/memetory/src/main/java/com/example/memetory/global/exception/advice/ExceptionAdvice.java
@@ -1,35 +1,46 @@
 package com.example.memetory.global.exception.advice;
 
-import org.springframework.http.HttpEntity;
+import static com.example.memetory.global.response.ErrorCode.*;
+import static org.springframework.http.HttpStatus.*;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.example.memetory.domain.member.exception.NotFoundMemberException;
-import com.example.memetory.domain.meme.exception.NotFoundMemeException;
-import com.example.memetory.global.security.jwt.exception.NotFoundEmailException;
-import com.example.memetory.global.security.jwt.exception.NotFoundTokenException;
+import com.example.memetory.global.exception.BusinessException;
+import com.example.memetory.global.response.ErrorCode;
+import com.example.memetory.global.response.ErrorResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestControllerAdvice
 public class ExceptionAdvice {
-	@ExceptionHandler(NotFoundTokenException.class)
-	ResponseEntity<HttpEntity> notFoundTokenException() {
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	@ExceptionHandler(Exception.class)
+	protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+		log.error(e.getMessage(), e);
+		ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+		return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
-	@ExceptionHandler(NotFoundEmailException.class)
-	ResponseEntity<HttpEntity> notFoundEmailException() {
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	@ExceptionHandler
+	protected ResponseEntity<ErrorResponse> handleRuntimeException(BusinessException e) {
+		final ErrorCode errorCode = e.getErrorCode();
+		final ErrorResponse response =
+			ErrorResponse.builder()
+				.errorMessage(errorCode.getMessage())
+				.build();
+		log.warn(e.getMessage());
+		return ResponseEntity.status(errorCode.getStatus()).body(response);
 	}
 
-	@ExceptionHandler(NotFoundMemberException.class)
-	ResponseEntity<HttpEntity> notFoundMemberException() {
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-	}
-
-	@ExceptionHandler(NotFoundMemeException.class)
-	ResponseEntity<HttpEntity> notFoundMemeException() {
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	@ExceptionHandler
+	protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+		MethodArgumentNotValidException e) {
+		final ErrorResponse response = ErrorResponse.of(INPUT_INVALID_VALUE, e.getBindingResult());
+		log.warn(e.getMessage());
+		return new ResponseEntity<>(response, BAD_REQUEST);
 	}
 }
