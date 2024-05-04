55fcc8910f702fe2d09b5f2c834c4a7c35b8047e
ggamD00 <jaehuek@naver.com>
2024-05-05 04:56:29 +0900

    [#73] MemesRepository에 밈스 전체 조회 구현
    Page보다 무한스크롤에 더 적합한 Slice 적용

--- backend/memetory/src/main/java/com/example/memetory/domain/memes/repository/MemesRepository.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/memes/repository/MemesRepository.java
@@ -1,7 +1,9 @@
 package com.example.memetory.domain.memes.repository;
 
+import com.example.memetory.domain.memes.dto.response.MemesResponse;
 import com.example.memetory.domain.memes.entity.Memes;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -15,6 +17,9 @@ public interface MemesRepository extends JpaRepository<Memes, Long> {
     @Query("SELECT ms FROM Memes ms WHERE ms.Id = :memesId")
     @EntityGraph(attributePaths = {"meme", "member"})
     Optional<Memes> findByMemesId(Long memesId);
+
+    @EntityGraph(attributePaths = {"meme", "member"})
+    Slice<Memes> findAllBy(Pageable pageable);
 
     @Query("SELECT ms FROM Memes ms ORDER BY ms.likeCount DESC")
     @EntityGraph(attributePaths = {"meme"})
