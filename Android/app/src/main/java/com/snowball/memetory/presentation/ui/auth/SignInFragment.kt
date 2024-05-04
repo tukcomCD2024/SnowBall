f5e4501cb5109c987b103daaa6553448d098d839
youngseo3 <wys4475@gmail.com>
2024-05-05 03:38:52 +0900

    [#71] SignInFragment 수정
    ViewModel 추가 및 네트워크 통신 작업 추가
    구글 엑세스 토큰을 받는 것이 성공하면 was에 다시 요청

--- Android/app/src/main/java/com/snowball/memetory/presentation/ui/auth/SignInFragment.kt
+++ Android/app/src/main/java/com/snowball/memetory/presentation/ui/auth/SignInFragment.kt
@@ -9,10 +9,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.common.api.ApiException
+import com.snowball.memetory.BuildConfig
 import com.snowball.memetory.R
+import com.snowball.memetory.data.api.AuthService
+import com.snowball.memetory.data.api.NetworkModule
+import com.snowball.memetory.data.api.NetworkModule.googleAuthService
+import com.snowball.memetory.data.dto.auth.request.GoogleTokenRequestDto
+import com.snowball.memetory.data.dto.auth.request.SignInRequestDto
+import com.snowball.memetory.data.repository.AuthRepository
 import com.snowball.memetory.databinding.FragmentSignInBinding
 import com.snowball.memetory.util.SocialLoginUtil
 
@@ -20,9 +28,12 @@ class SignInFragment : Fragment() {
 
     private var _binding: FragmentSignInBinding? = null
     private val binding get() = _binding!!
-
+//    private lateinit var authService: AuthService
+    private lateinit var authViewModel: AuthViewModel
 
     private lateinit var socialLoginUtil: SocialLoginUtil
+    private var clientSecPwd = BuildConfig.GOOGLE_LOGIN_CLIENT_SECURITY_PASSWORD
+
     private var googleLoginLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         Log.d("SignInFragment", "$result")
         val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
@@ -33,6 +44,14 @@ class SignInFragment : Fragment() {
             Toast.makeText(context, "${account.idToken}", Toast.LENGTH_LONG).show()
             Log.d("SocialLoginUtil","email =${account.email}, id=${account.id}\n token =${account.idToken}" +
                     "\nserverAuthCode=${account.serverAuthCode}")
+//            Log.e("AuthService", "${GoogleTokenRequestDto("authorization_code", BuildConfig.GOOGLE_LOGIN_CLIENT_ID,
+//                clientSecPwd,"", account.serverAuthCode!!)}")
+            authViewModel.requestAccessToken(GoogleTokenRequestDto("authorization_code", BuildConfig.GOOGLE_LOGIN_CLIENT_ID,
+                clientSecPwd,"", account.serverAuthCode!!))
+
+//             서버로 엑세스토큰 전송
+//            authViewModel.loginUser(SignInRequestDto(account.serverAuthCode!!, "GOOGLE"))
+
 //            sendLoginDataToServer(account.idToken)
 //            googleSignOut()
         } catch (e: ApiException) {
@@ -68,6 +87,33 @@ class SignInFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val authService = NetworkModule.authService
+        val googleAuthService = NetworkModule.googleAuthService
+        val authRepository = AuthRepository(authService, googleAuthService)
+
+        val factory = AuthViewModelFactory(authRepository)
+        authViewModel = ViewModelProvider(this, factory).get(AuthViewModel::class.java)
+
+        authViewModel.loginResult.observe(viewLifecycleOwner) {
+            it.onSuccess {
+                Log.d("AUTH", "AccessToken = ${it.accessToken}, RefreshToken = ${it.refreshToken}")
+                findNavController().navigate(R.id.signUpFragment)
+
+            }
+            it.onFailure {
+                Log.d("AUTH", "${it.message}")
+                Toast.makeText(context, "Login failed: ${it.message}", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        // AuthService와 AuthRepository 초기화
+//        authService = NetworkModule.retrofit.create(AuthService::class.java)
+//        val authRepository = AuthRepository(authService)
+//
+//        // ViewModelProvider를 사용하여 AuthViewModel 초기화
+//        val factory = AuthViewModelFactory(authRepository)
+//        authViewModel = ViewModelProvider(this, factory).get(AuthViewModel::class.java)
 
         // SocialLoginUtil 초기화
         socialLoginUtil = SocialLoginUtil(requireContext(), object : SocialLoginUtil.LoginCallback {
