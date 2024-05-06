c716170cc59d31ecfb4ce7546753526871b9edef
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2024-05-06 23:18:04 +0900

    [#23] Encodable 추가

---
+++ iOS/Memetory/Memetory/Networking/Foundation/Encodable+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  Encodable+Extension.swift
+//  Memetory
+//
+//  Created by 이승진 on 2024/05/02.
+//
+
+import Foundation
+
+extension Encodable {
+    func toDictionary() -> [String: Any] {
+        guard let data = try? JSONEncoder().encode(self),
+              let jsonData = try? JSONSerialization.jsonObject(with: data),
+              let dictionaryData = jsonData as? [String: Any] else { return [:] }
+        return dictionaryData
+    }
+}
