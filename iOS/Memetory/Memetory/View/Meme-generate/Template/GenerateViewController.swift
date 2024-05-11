b1a546b0be614d3c78e2c949b5fcd22612902ae0
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2024-05-12 01:15:54 +0900

    [#135] 밈 생성 UI 수정

--- iOS/Memetory/Memetory/View/Meme-generate/Template/GenerateViewController.swift
+++ iOS/Memetory/Memetory/View/Meme-generate/Template/GenerateViewController.swift
@@ -7,18 +7,32 @@
 
 import UIKit
 import SnapKit
+import RxSwift
 
 final class GenerateViewController: UIViewController {
     
-    private let tableView = UITableView()
+    private let templateTitle = Scene.templateTitle
+    
+    var disposeBag = DisposeBag()
     
     let titleLabel: UILabel = {
         let label = UILabel()
         label.text = "템플릿 선택"
         label.textColor = .black
         label.font = UIFont(name: "Pretendard-Bold", size: 25)
-//        label.font = .boldSystemFont(ofSize: 25)
         return label
+    }()
+    
+    lazy var gridView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width / 2 - 20, height: 225)
+        layout.minimumInteritemSpacing = 10
+        
+        let grid = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        grid.register(MainTemplateCollectionViewCell.self, forCellWithReuseIdentifier: MainTemplateCollectionViewCell.cellId)
+        grid.showsVerticalScrollIndicator = false
+        return grid
     }()
 
     override func viewDidLoad() {
@@ -33,20 +47,13 @@ final class GenerateViewController: UIViewController {
     
     func setupTableView() {
         // 델리게이트 패턴 대리자 설정
-        tableView.dataSource = self
-        tableView.delegate = self
-        // 셀의 높이 설정
-        tableView.rowHeight = 300
-        
-        // 셀의 등록 과정(스토리보드 사용시에는 스토리보드에서 자동 등록)
-        tableView.register(TemplateTableViewCell.self, forCellReuseIdentifier: "TemplateCell")
-        
-//        tableView.backgroundColor = .gray
+        gridView.delegate = self
+        gridView.dataSource = self
     }
     
     private func setViews() {
         view.addSubview(titleLabel)
-        view.addSubview(tableView)
+        view.addSubview(gridView)
     }
     
     private func setConstrainsts() {
@@ -55,29 +62,32 @@ final class GenerateViewController: UIViewController {
             make.leading.equalToSuperview().offset(15)
         }
         
-        tableView.snp.makeConstraints { make in
-            make.top.equalTo(titleLabel.snp.bottom).offset(20)
-            make.leading.equalToSuperview()
-            make.trailing.equalToSuperview()
-            make.bottom.equalToSuperview()
+        gridView.snp.makeConstraints{
+            $0.leading.equalToSuperview().offset(15)
+            $0.trailing.equalToSuperview().offset(-15)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(30)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
         }
     }
 }
 
-extension GenerateViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+extension GenerateViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        self.items.count
+        return templateTitle.count
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TemplateCell", for: indexPath) as! TemplateTableViewCell
-        cell.selectionStyle = .none
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MainTemplateCollectionViewCell.cellId, for: indexPath) as! MainTemplateCollectionViewCell
+//        cell.prepare(color: self.items[indexPath.item])
+        cell.titleLabel.text = templateTitle[indexPath.item]
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let detailVC = TemSelectViewController()
-//        detailVC.movieData = moviesArray[indexPath.row]
-        show(detailVC, sender: nil)
+//        settingVC.delegate = self
+        
+//        let array = memberListManager.getMemberList()
+//        detailVC.member = array[indexPath.row]
+        navigationController?.pushViewController(detailVC, animated: true)
     }
 }
---
+++ iOS/Memetory/Memetory/View/Meme-generate/Template/MainTemplateCollectionViewCell.swift
@@ -0,0 +1,84 @@
+//
+//  MainTemplateCollectionViewCell.swift
+//  Memetory
+//
+//  Created by 이승진 on 2024/05/11.
+//
+
+import UIKit
+import RxSwift
+
+class MainTemplateCollectionViewCell: UICollectionViewCell {
+    static let cellId = "MainTemplateCollectionViewCell"
+    
+    var disposeBag = DisposeBag()
+    
+    let mainImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "demo1")
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 15
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    let titleLabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont(name: "Pretendard-Bold", size: 15)
+        return label
+    }()
+    
+    var bag = DisposeBag()
+    
+    override init(frame : CGRect){
+        super.init(frame: frame)
+        
+        self.backgroundColor = .black
+        setUp()
+        setConstraints()
+    }
+    
+//    public func bind(log: Log, isBigCell : Bool){
+//        self.title.text = log.title
+//        self.date.text = log.getTravelPeriod()
+//        dDay.setTitle(dateController.days(from:log.startDate), for: .normal)
+//        
+//        self.backImage.kf.setImage(with: URL(string: log.imageUrl))
+//        self.backImage.alpha = 0.5
+//    
+//        setConst(isBigCell)
+//        setSize(isBigCell)
+//    }
+//    
+//    private func setSize(_ isBigCell : Bool){
+//        title.font = WithYouFontFamily.Pretendard.bold.font(size: isBigCell ? 22 : 16)
+//        date.font = WithYouFontFamily.Pretendard.medium.font(size: isBigCell ? 15 : 12)
+//        //Only for big cell
+//        dDay.titleLabel?.font = WithYouFontFamily.Pretendard.bold.font(size: 15)
+//    }
+    
+    private func setUp(){
+        self.clipsToBounds = true
+//        self.addSubview(mainImageView)
+        self.addSubview(titleLabel)
+    }
+    
+    func setConstraints() {
+//        mainImageView.snp.makeConstraints { make in
+//            make.height.width.equalTo(270)
+//            make.top.equalTo(self.snp.top).offset(20)
+//            make.centerY.equalTo(self.snp.centerY)
+//            make.centerX.equalTo(self.snp.centerX)
+//            
+//        }
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(self.snp.centerX)
+            make.centerY.equalTo(self.snp.centerY)
+        }
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
