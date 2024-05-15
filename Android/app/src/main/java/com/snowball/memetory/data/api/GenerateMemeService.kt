0a8d2cfc905915f81a1038cc528455be45b50908
youngseo3 <wys4475@gmail.com>
2024-05-16 01:08:39 +0900

    [#25] GenerateMemeService getVoiceIdList() 추가
    일레븐랩스 기본 목소리 가져오기 API

--- Android/app/src/main/java/com/snowball/memetory/data/api/GenerateMemeService.kt
+++ Android/app/src/main/java/com/snowball/memetory/data/api/GenerateMemeService.kt
@@ -1,7 +1,9 @@
 package com.snowball.memetory.data.api
 
+import com.snowball.memetory.data.dto.ResponseBody
 import com.snowball.memetory.data.dto.generatememe.voice.request.VoiceIdRequestDto
 import com.snowball.memetory.data.dto.generatememe.voice.response.VoiceIdResponseDto
+import com.snowball.memetory.domain.model.voice.VoiceList
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -10,5 +12,12 @@ import retrofit2.http.POST
 interface GenerateMemeService {
     @POST("/voice")
     suspend fun extractVoice(@Body voiceInfo: VoiceIdRequestDto): Response<VoiceIdResponseDto>
+
+    @GET("/voice/library") // 일레븐랩스 기본 목소리 가져오기
+    suspend fun getVoiceIdList(): Response<ResponseBody<VoiceList>>
+
+    //    @GET("/voice/member") 단일 조회
+//    suspend fun getVoiceId(): Response<ResponseBody<VoiceList>>
+
 
 }
