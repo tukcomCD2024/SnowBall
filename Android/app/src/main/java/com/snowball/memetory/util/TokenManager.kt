8b8a988787f731544b7495013cfb4e8b822f44eb
youngseo3 <wys4475@gmail.com>
2024-05-15 15:47:18 +0900

    [#25] TokenManager 추가
    EncryptedSharedPreferences를 사용하여 로그인 후 엑세스 토큰 및 리프레쉬 토큰 관리

---
+++ Android/app/src/main/java/com/snowball/memetory/util/TokenManager.kt
@@ -0,0 +1,36 @@
+package com.snowball.memetory.util
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+
+object TokenManager {
+    private lateinit var context: Context
+    private lateinit var encryptedSharedPreferences: SharedPreferences
+
+    fun init(context: Context) {
+        this.context = context
+        val masterKey = MasterKey.Builder(context)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        encryptedSharedPreferences = EncryptedSharedPreferences.create(
+            context,
+            "secure_auth_prefs",
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+
+    fun saveTokens(accessToken: String, refreshToken: String) {
+        val editor = encryptedSharedPreferences.edit()
+        editor.putString("ACCESS_TOKEN", accessToken)
+        editor.putString("REFRESH_TOKEN", refreshToken)
+        editor.apply()
+    }
+
+    fun getAccessToken(): String? = encryptedSharedPreferences.getString("ACCESS_TOKEN", null)
+    fun getRefreshToken(): String? = encryptedSharedPreferences.getString("REFRESH_TOKEN", null)
+}
