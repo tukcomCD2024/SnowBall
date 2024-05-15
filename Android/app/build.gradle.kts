cda162a597ada1db1179bc9fee0c4396d7e48e31
youngseo3 <wys4475@gmail.com>
2024-05-15 17:16:33 +0900

    [#25] build.gradle 의존성 추가
    EncryptedSharedPreferences

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -107,4 +107,9 @@ dependencies {
     implementation("com.google.gms:google-services:4.3.15")
     implementation("com.google.firebase:firebase-auth:22.0.0")
     implementation("com.google.firebase:firebase-bom:32.0.0")
+
+    // Jetpack Security
+    implementation("androidx.security:security-crypto-ktx:1.1.0-alpha06")
+    
+    implementation("com.google.android.gms:play-services-basement:18.3.0")
 }
