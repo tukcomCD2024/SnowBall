70fe33dd5ed0d0de345b096fbd17ef6ed4b0d25b
ggamD00 <jaehuek@naver.com>
2024-05-15 18:13:04 +0900

    [#87] VoiceRepository 멤버별 목소리 조회 기능 구현

--- backend/memetory/src/main/java/com/example/memetory/domain/voice/repository/VoiceRepository.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/voice/repository/VoiceRepository.java
@@ -3,5 +3,5 @@ package com.example.memetory.domain.voice.repository;
 import com.example.memetory.domain.voice.entity.Voice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VoiceRepository extends JpaRepository<Voice, Long> {
+public interface VoiceRepository extends JpaRepository<Voice, Long>, VoiceQueryRepository{
 }
