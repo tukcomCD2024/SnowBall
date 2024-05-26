8e6125f4563c8a06eb337b50d625ed54f70f795f
JunRain <sa2313@tukorea.ac.kr>
2024-05-26 20:02:21 +0900

    [#92] RedisConfig에 RedisTamplate 빈 추가

--- backend/memetory/src/main/java/com/example/memetory/global/config/RedisConfig.java
+++ backend/memetory/src/main/java/com/example/memetory/global/config/RedisConfig.java
@@ -1,12 +1,21 @@
 package com.example.memetory.global.config;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
 	@Value("${spring.redis.host}")
 	private String host;
@@ -16,6 +25,22 @@ public class RedisConfig {
 
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(host, port);
+		return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+	}
+
+	@Bean(name = "rankingRedisTemplate")
+	public RedisTemplate<String, Long> rankingRedisTemplate() {
+		RedisTemplate<String, Long> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+		return redisTemplate;
+	}
+
+	@Bean(name = "rankingZSetOperations")
+	public ZSetOperations<String, Long> rankingZSetOperations(
+		@Qualifier ("rankingRedisTemplate") RedisTemplate<String, Long> redisTemplate) {
+		return redisTemplate.opsForZSet();
 	}
 }
