ef13c2bd10c986b034ab1749717f411745550574
JunRain <sa2313@tukorea.ac.kr>
2024-05-27 23:31:09 +0900

    [#92] likeService에 rankingService 의존성 추가로 인한 Test 코드에 rankingService 의존성 추가

--- backend/memetory/src/main/java/com/example/memetory/domain/like/dto/LikeServiceDto.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/like/dto/LikeServiceDto.java
@@ -1,8 +1,5 @@
 package com.example.memetory.domain.like.dto;
 
-import com.example.memetory.domain.like.entity.Like;
-import com.example.memetory.domain.member.entity.Member;
-import com.example.memetory.domain.memes.entity.Memes;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,13 +8,13 @@ import lombok.Getter;
 @Builder
 @AllArgsConstructor
 public class LikeServiceDto {
-    private Long memesId;
-    private String email;
+	private Long memesId;
+	private String email;
 
-    public static LikeServiceDto fromEmailAndMemesId(String email, Long memesId) {
-        return LikeServiceDto.builder()
-                .email(email)
-                .memesId(memesId)
-                .build();
-    }
+	public static LikeServiceDto fromEmailAndMemesId(String email, Long memesId) {
+		return LikeServiceDto.builder()
+			.email(email)
+			.memesId(memesId)
+			.build();
+	}
 }
