4b494ecbf9524f4156ba0b502c1ee4770498ed3f
JunRain <sa2313@tukorea.ac.kr>
2024-05-27 15:22:50 +0900

    [#92] RankingService 주간/월간 조회 구현

--- backend/memetory/src/main/java/com/example/memetory/domain/memes/service/RankingService.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/memes/service/RankingService.java
@@ -1,43 +1,34 @@
 package com.example.memetory.domain.memes.service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class RankingService {
 	private final String PREFIX = "LIKE_RANKING_DATE::";
+	private final String POSTFIX_WEEK = "::WEEK";
+	private final String POSTFIX_MONTH = "::MONTH";
+	private final Long TOP_TEN = 10L;
+
 	private final ZSetOperations<String, Long> rankingZSet;
 
 	@Transactional
 	public void increaseTodayMemesLikeCountFromMemesId(Long memesId) {
 		String key = PREFIX + LocalDate.now();
 
-		if (isExistedTodayMemesId(key, memesId)) {
-			rankingZSet.incrementScore(key, memesId, 1);
-			return;
-		}
-		saveTodayMemesLikeCount(key, memesId);
+		rankingZSet.incrementScore(key, memesId, 1);
 	}
 
-	private boolean isExistedTodayMemesId(String key, Long memesId) {
-		Double score = rankingZSet.score(key, memesId);
-
-		if (score == null) {
-			return false;
-		}
-		return true;
-	}
-
-	private void saveTodayMemesLikeCount(String key, Long memesId) {
-		rankingZSet.add(key, memesId, 1);
-	}
-
+	// Todo 음수를 확인하는 로직 추가 필요해 보임
 	@Transactional
 	public void decreaseTodayMemesLikeCountFromMemesId(Long memesId) {
 		String key = PREFIX + LocalDate.now();
@@ -45,7 +36,44 @@ public class RankingService {
 		rankingZSet.incrementScore(key, memesId, -1);
 	}
 
-	// 현재 - 일주일 전(Key) 랭킹을 구함
+	@Transactional(readOnly = true)
+	public List<Long> findTopTenMemesLikeCountForWeek() {
+		String key = PREFIX + LocalDate.now() + POSTFIX_WEEK;
 
-	// 현재 - 한달 전(Key) 랭킹을 구함
+		if (isNotExistedKey(key))
+			unionMemesFromKeyAndDay(key, 7);
+
+		Set<Long> result = rankingZSet.reverseRange(key, 0, TOP_TEN);
+		return List.copyOf(result);
+	}
+
+	private boolean isNotExistedKey(String key) {
+		Set<Long> check = rankingZSet.range(key, 0, 1);
+
+		return check.isEmpty();
+	}
+
+	private void unionMemesFromKeyAndDay(String key, int day) {
+		List<String> keyList = new ArrayList<>();
+		LocalDate today = LocalDate.now();
+
+		for (int i = 1; i < day; i++) {
+			LocalDate date = today.minusDays(i);
+			keyList.add(PREFIX + date);
+		}
+
+		rankingZSet.unionAndStore(key, keyList, key);
+	}
+
+	@Transactional(readOnly = true)
+	public List<Long> findTopTenMemesLikeCountForMonth() {
+		String key = PREFIX + LocalDate.now() + POSTFIX_MONTH;
+
+		if (isNotExistedKey(key)) {
+			unionMemesFromKeyAndDay(key, 30);
+		}
+
+		Set<Long> result = rankingZSet.reverseRange(key, 0, TOP_TEN);
+		return List.copyOf(result);
+	}
 }
--- backend/memetory/src/test/java/com/example/memetory/domain/memes/service/RankingServiceTest.java
+++ backend/memetory/src/test/java/com/example/memetory/domain/memes/service/RankingServiceTest.java
@@ -3,43 +3,45 @@ package com.example.memetory.domain.memes.service;
 import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.ZSetOperations;
-
-import jakarta.transaction.Transactional;
 
 @DisplayName("Ranking 서비스 테스트의 ")
 @SpringBootTest
-@Transactional
 public class RankingServiceTest {
-	private static final String PREFIX = "LIKE_RANKING_DATE::";
-	static String key;
-
+	private final String PREFIX = "LIKE_RANKING_DATE::";
+	private final String POSTFIX_WEEK = "::WEEK";
 	private final Long MEMES_ID = -1L;
+
+	private String key;
 
 	@Autowired
 	private RankingService rankingService;
 	@Autowired
 	private ZSetOperations<String, Long> rankingZSet;
+	@Autowired
+	private RedisConnectionFactory redisConnectionFactory;
 
-	@BeforeAll
-	public static void setUp() {
+	@BeforeEach
+	public void setUp() {
 		key = PREFIX + LocalDate.now();
 	}
 
 	@AfterEach
 	void clearRedis() {
-		rankingZSet.remove(key, MEMES_ID);
+		redisConnectionFactory.getConnection().flushAll();
 	}
 
 	@Test
-	@DisplayName("존재하지 않는 memes와 memesId를 통한 score 증가 성송")
+	@DisplayName("존재하지 않는 memes와 memesId를 통한 score 증가 성공")
 	void Given_memesId_When_increaseTodayMemesLikeCountFromMemesId_Then_Memems_Score() {
 		// given
 		Double expectedScore = 1.0;
@@ -81,4 +83,44 @@ public class RankingServiceTest {
 		assertThat(rankingZSet.score(key, MEMES_ID)).isEqualTo(expectedScore);
 	}
 
+	@Test
+	@DisplayName("존재하지 않는 주간 key를 통한 MemesIdList 반환")
+	void Given_NotExistKey_When_findTopTenMemesLikeCountForWeek_Then_MemesIdList() {
+		// given
+		final int DAY = 7;
+
+		for (int i = 0; i < DAY; i++) {
+			LocalDate date = LocalDate.now().minusDays(i);
+			String dateKey = PREFIX + date;
+			rankingZSet.add(dateKey, (long)i, i);
+		}
+
+		// when
+		List<Long> result = rankingService.findTopTenMemesLikeCountForWeek();
+
+		//then
+		assertThat(result.size()).isEqualTo(DAY - 1);
+		assertThat(result.get(0)).isEqualTo(DAY - 1);
+	}
+
+	@Test
+	@DisplayName("존재하는 주간 key를 통한 MemesIdList 반환")
+	void Given_ExistKey_When_findTopTenMemesLikeCountForWeek_Then_MemesIdList() {
+		// given
+		key += POSTFIX_WEEK;
+		rankingZSet.add(key, 7L, 7);
+
+		for (int i = 0; i < 7; i++) {
+			LocalDate date = LocalDate.now().minusDays(i);
+			String dateKey = PREFIX + date;
+			rankingZSet.add(dateKey, (long)i, i);
+		}
+
+		// when
+		List<Long> result = rankingService.findTopTenMemesLikeCountForWeek();
+
+		//then
+		assertThat(result.size()).isEqualTo(1);
+		assertThat(result.get(0)).isEqualTo(7L);
+	}
 }
