cc0d5c6657e02aaac9b3923952ba2bacf6248b8d
JunRain <sa2313@tukorea.ac.kr>
2024-05-31 15:32:55 +0900

    [#94] Oauth2ProviderService로 Provider URL을 설정 파일에서 주입받도록 설정

--- backend/memetory/src/main/java/com/example/memetory/domain/member/entity/SocialType.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/member/entity/SocialType.java
@@ -8,10 +8,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum SocialType {
-	GOOGLE("https://www.googleapis.com/oauth2/v2/userinfo"),
-	KAKAO("https://kapi.kakao.com/v2/user/me");
-
-	private final String providerUrl;
+	GOOGLE(),
+	KAKAO();
 
 	@JsonCreator
 	public static SocialType from(String s) {
