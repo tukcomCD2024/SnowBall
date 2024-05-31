f344ad5543982e6eece226f36321706b33ea0190
JunRain <sa2313@tukorea.ac.kr>
2024-05-31 20:56:29 +0900

    [#94] Auth 테스트용 팩토리 객체 구현

---
+++ backend/memetory/src/test/java/com/example/memetory/domain/auth/AuthFixture.java
@@ -0,0 +1,41 @@
+package com.example.memetory.domain.auth;
+
+import java.util.Map;
+
+import com.example.memetory.domain.auth.dto.LoginRequest;
+import com.example.memetory.domain.member.entity.SocialType;
+import com.example.memetory.global.security.jwt.dto.TokenResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class AuthFixture {
+	static ObjectMapper mapper = new ObjectMapper();
+
+	public static String JSON_GOOGLE_LOGIN_REQUEST() throws JsonProcessingException {
+		return mapper.writeValueAsString(GOOGLE_LOGIN_REQUEST());
+	}
+
+	public static LoginRequest GOOGLE_LOGIN_REQUEST() {
+		return new LoginRequest("google login token", SocialType.GOOGLE);
+	}
+
+	public static String JSON_TOKEN_RESPONSE() throws JsonProcessingException {
+		return mapper.writeValueAsString(TOKEN_RESPONSE());
+	}
+
+	public static TokenResponse TOKEN_RESPONSE() {
+		return TokenResponse.builder()
+			.accessToken("access token")
+			.refreshToken("refresh token")
+			.build();
+	}
+
+	public static String JSON_GOOGLE_OAUTH2_RESPONSE() throws JsonProcessingException {
+		Map<String, String> googleResponse = Map.ofEntries(
+			Map.entry("id", "123456789"),
+			Map.entry("name", "이준우"),
+			Map.entry("picture", "urlS3"));
+
+		return mapper.writeValueAsString(googleResponse);
+	}
+}
