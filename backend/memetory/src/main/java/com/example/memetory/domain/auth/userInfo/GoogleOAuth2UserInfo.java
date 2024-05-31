fdeb64f10d5936775241d730375b571d6c534738
JunRain <sa2313@tukorea.ac.kr>
2024-05-31 20:55:38 +0900

    [#94] 사용하지 않는 로깅 어노테이션 제거

--- backend/memetory/src/main/java/com/example/memetory/domain/auth/userInfo/GoogleOAuth2UserInfo.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/auth/userInfo/GoogleOAuth2UserInfo.java
@@ -2,9 +2,6 @@ package com.example.memetory.domain.auth.userInfo;
 
 import java.util.Map;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class GoogleOAuth2UserInfo extends OAuth2UserInfo {
 
 	public GoogleOAuth2UserInfo(Map<String, Object> attributes) {
