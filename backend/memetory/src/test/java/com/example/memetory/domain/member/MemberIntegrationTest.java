c7307074acf05bc07e6cdda0bea777c9bfc77af4
JunRain <sa2313@tukorea.ac.kr>
2024-06-01 16:26:10 +0900

    [#94] 멤버 통합 테스트 멤버 업데이트 성공 구현

---
+++ backend/memetory/src/test/java/com/example/memetory/domain/member/MemberIntegrationTest.java
@@ -0,0 +1,76 @@
+package com.example.memetory.domain.member;
+
+import static com.example.memetory.domain.member.MemberFixture.*;
+import static io.restassured.RestAssured.*;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.example.memetory.domain.member.dto.request.MemberUpdateRequest;
+import com.example.memetory.domain.member.dto.response.MemberResponse;
+import com.example.memetory.domain.member.entity.Member;
+import com.example.memetory.domain.member.repository.MemberRepository;
+import com.example.memetory.global.integration.BaseIntegrationTest;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+public class MemberIntegrationTest extends BaseIntegrationTest {
+	@Autowired
+	private MemberRepository memberRepository;
+
+	private Member member;
+	private String accessToken;
+
+	@Value("${jwt.secretKey}")
+	private String secretKey;
+
+	@BeforeEach
+	void setUpAccessToken() {
+		Date now = new Date();
+		member = memberRepository.save(MEMBER());
+
+		accessToken = JWT.create()
+			.withSubject("AccessToken")
+			.withExpiresAt(new Date(now.getTime() + 18000))
+			.withClaim("email", member.getEmail())
+			.sign(Algorithm.HMAC512(secretKey));
+	}
+
+	@Test
+	@DisplayName("MemberUpdateRequest을 통한 MemberResponse 반환 성공")
+	void Given_MemberUpdateRequest_When_updateMember_Then_MemberUpdateResponse() {
+		// given
+		MemberUpdateRequest request = MEMBER_UPDATE_REQUEST();
+
+		// when
+		ExtractableResponse<Response> response =
+			given()
+				.log()
+				.all()
+				.auth().oauth2(accessToken)
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.body(request)
+				.when()
+				.post("/member")
+				.then()
+				.log()
+				.all()
+				.extract();
+
+		MemberResponse result = response.jsonPath().getObject("data", MemberResponse.class);
+		Member updateMember = memberRepository.findById(member.getId()).get();
+
+		// then
+		assertThat(result).usingRecursiveComparison().isEqualTo(MemberResponse.of(updateMember));
+	}
+}
