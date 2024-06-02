e6e2c3375409246ab19f7ef5c9f95597f8b331a1
JunRain <sa2313@tukorea.ac.kr>
2024-06-02 20:55:17 +0900

    [#94] JwtService에 JwtUtil 의존성 추가 및 리팩토링

--- backend/memetory/src/main/java/com/example/memetory/global/security/jwt/service/JwtService.java
+++ backend/memetory/src/main/java/com/example/memetory/global/security/jwt/service/JwtService.java
@@ -1,20 +1,16 @@
 package com.example.memetory.global.security.jwt.service;
 
 import java.io.IOException;
-import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.memetory.domain.member.repository.MemberRepository;
 import com.example.memetory.global.security.jwt.dto.TokenResponse;
-import com.example.memetory.global.security.jwt.exception.NotFoundEmailException;
 import com.example.memetory.global.security.jwt.exception.NotFoundTokenException;
 import com.example.memetory.global.security.jwt.refresh.service.RefreshTokenService;
+import com.example.memetory.global.security.jwt.util.JwtUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -26,21 +22,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public class JwtService {
-	private static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
-	private static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
-	private static final String EMAIL_CLAIM = "email";
 	private static final String BEARER = "Bearer ";
 
 	private final MemberRepository memberRepository;
 	private final RefreshTokenService refreshTokenService;
 	private final ObjectMapper objectMapper;
+	private final JwtUtil jwtUtil;
 
-	@Value("${jwt.secretKey}")
-	private String secretKey;
-	@Value("${jwt.access.expiration}")
-	private Long accessTokenExpirationPeriod;
-	@Value("${jwt.refresh.expiration}")
-	private Long refreshTokenExpirationPeriod;
 	@Value("${jwt.access.header}")
 	private String accessHeader;
 	@Value("${jwt.refresh.header}")
@@ -48,9 +36,11 @@ public class JwtService {
 
 	public void sendAccessAndRefreshToken(HttpServletResponse response, String email) {
 		try {
-			String refreshToken = createRefreshToken();
+			String refreshToken = jwtUtil.generateRefreshToken();
+			String accessToken = jwtUtil.generateAccessToken(email);
+
 			String token = objectMapper.writeValueAsString(TokenResponse.builder()
-				.accessToken(createAccessToken(email))
+				.accessToken(accessToken)
 				.refreshToken(refreshToken)
 				.build());
 
@@ -62,53 +52,28 @@ public class JwtService {
 
 	}
 
-	private String createAccessToken(String email) {
-		Date now = new Date();
-		return JWT.create()
-			.withSubject(ACCESS_TOKEN_SUBJECT)
-			.withExpiresAt(new Date(now.getTime() + accessTokenExpirationPeriod))
-			.withClaim(EMAIL_CLAIM, email)
-			.sign(Algorithm.HMAC512(secretKey));
-	}
-
-	private String createRefreshToken() {
-		Date now = new Date();
-		return JWT.create()
-			.withSubject(REFRESH_TOKEN_SUBJECT)
-			.withExpiresAt(new Date(now.getTime() + refreshTokenExpirationPeriod))
-			.sign(Algorithm.HMAC512(secretKey));
-	}
-
-	public Optional<String> extractRefreshToken(HttpServletRequest request) {
+	public String extractRefreshToken(HttpServletRequest request) {
 		return Optional.ofNullable(request.getHeader(refreshHeader))
-			.map(token -> token.replace("Bearer ", ""));
+			.map(token -> token.replace(BEARER, ""))
+			.orElseThrow(NotFoundTokenException::new);
 	}
 
-	public Optional<String> extractAccessToken(HttpServletRequest request) {
+	public String extractAccessToken(HttpServletRequest request) {
 		return Optional.ofNullable(request.getHeader(accessHeader))
-			.map(token -> token.replace("Bearer ", ""));
-	}
-
-	public String extractEmail(String accessToken) throws JWTVerificationException {
-		return Optional.of(
-			JWT.require(Algorithm.HMAC512(secretKey))
-				.build()
-				.verify(accessToken)
-				.getClaim(EMAIL_CLAIM).asString())
-			.orElseThrow(NotFoundEmailException::new);
+			.map(token -> token.replace(BEARER, ""))
+			.orElseThrow(NotFoundTokenException::new);
 	}
 
 	public String getEmail(HttpServletRequest request) {
-		String accessToken = this.extractAccessToken(request).orElseThrow(NotFoundTokenException::new);
+		String accessToken = this.extractAccessToken(request);
 		return this.extractEmail(accessToken);
 	}
 
+	public String extractEmail(String accessToken) {
+		return jwtUtil.extractEmailFromAccessToken(accessToken);
+	}
+
 	public boolean isTokenValid(String token) {
-		try {
-			JWT.require(Algorithm.HMAC512(secretKey)).build().verify(token);
-			return true;
-		} catch (Exception e) {
-			return false;
-		}
+		return jwtUtil.isTokenValid(token);
 	}
 }
