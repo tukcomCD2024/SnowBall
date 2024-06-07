cdf88e56fe998d9a348ce494eaee81200ae2f9d7
JunRain <sa2313@tukorea.ac.kr>
2024-06-07 14:11:58 +0900

    [#58] MemeService 알림 보내는 로직 리팩토링

--- backend/memetory/src/main/java/com/example/memetory/domain/meme/service/MemeService.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/meme/service/MemeService.java
@@ -1,6 +1,6 @@
 package com.example.memetory.domain.meme.service;
 
-import java.time.LocalDateTime;
+import static com.example.memetory.global.firebase.FirebaseMessage.*;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
@@ -21,11 +21,7 @@ import com.example.memetory.domain.meme.entity.Meme;
 import com.example.memetory.domain.meme.exception.AccessDeniedMemeException;
 import com.example.memetory.domain.meme.exception.NotFoundMemeException;
 import com.example.memetory.domain.meme.repository.MemeRepository;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
+import com.example.memetory.global.firebase.service.FirebaseService;
 import com.google.gson.Gson;
 
 import lombok.RequiredArgsConstructor;
@@ -37,7 +33,7 @@ import lombok.extern.slf4j.Slf4j;
 public class MemeService {
 	private final MemberService memberService;
 	private final MemeRepository memeRepository;
-	private final FirebaseApp firebaseApp;
+	private final FirebaseService firebaseService;
 
 	@Value("${spring.ai-server.url}")
 	private String aiServerUrl;
@@ -49,25 +45,7 @@ public class MemeService {
 
 		Meme savedMeme = memeRepository.save(meme);
 
-		// TODO FCM을 통한 알림 전송 구현
-		Message message = Message.builder()
-			.setToken(member.getFcmToken())
-			.setNotification(
-				Notification.builder()
-					.setTitle("밈 생성 완료")
-					.setBody("밈 생성이 완료되었습니다.")
-					.build()
-			)
-			.putData("time", LocalDateTime.now().toString())
-			.build();
-
-
-		try {
-			String response = FirebaseMessaging.getInstance(firebaseApp).send(message);
-			log.info("Sent message: {}", response);
-		} catch (FirebaseMessagingException e) {
-			log.error("cannot send message by token. error info : {}", e.getMessage());
-		}
+		firebaseService.sendMessage(MEME_CREATE_MESSAGE.toMessageWithFcmToken(member.getFcmToken()));
 
 		return MemeResponse.of(savedMeme);
 	}
