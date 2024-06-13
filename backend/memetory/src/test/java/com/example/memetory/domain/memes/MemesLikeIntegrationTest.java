7e3e4348a85a7069f9fd7d42a6dc0a2955df1aa3
JunRain <sa2313@tukorea.ac.kr>
2024-06-13 20:51:53 +0900

    [#94] MemesLikeIntegrationTest memesId를 통한 좋아요 삭제 성공

--- backend/memetory/src/test/java/com/example/memetory/domain/memes/MemesLikeIntegrationTest.java
+++ backend/memetory/src/test/java/com/example/memetory/domain/memes/MemesLikeIntegrationTest.java
@@ -7,20 +7,13 @@ import static com.example.memetory.global.response.ErrorCode.*;
 import static com.example.memetory.global.response.ResultCode.*;
 import static io.restassured.RestAssured.*;
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.ResultActions;
 
-import com.example.memetory.domain.like.dto.LikeServiceDto;
-import com.example.memetory.domain.like.exception.NotCreateLikeException;
+import com.example.memetory.domain.like.entity.Like;
 import com.example.memetory.domain.like.repository.LikeRepository;
 import com.example.memetory.domain.meme.entity.Meme;
 import com.example.memetory.domain.meme.repository.MemeRepository;
@@ -39,12 +32,21 @@ public class MemesLikeIntegrationTest extends BaseIntegrationTest {
 	@Autowired
 	LikeRepository likeRepository;
 
+	private Meme meme;
+	private Memes memes;
+
+	@Override
+	@BeforeEach
+	public void setUp() {
+		super.setUp();
+
+		meme = memeRepository.save(MEME(member));
+		memes = memesRepository.save(MEMES(member, meme));
+	}
+
 	@Test
 	@DisplayName("memesId를 통한 좋아요 등록 성공")
 	void Given_memesId_When_registerLike_Then_CREATE_LIKE_SUCCESS() {
-		Meme meme = memeRepository.save(MEME(member));
-		Memes memes = memesRepository.save(MEMES(member, meme));
-
 		// when
 		ExtractableResponse<Response> response =
 			given()
@@ -70,8 +72,6 @@ public class MemesLikeIntegrationTest extends BaseIntegrationTest {
 	@DisplayName("DB에 존재하는 좋아요로 인한 LIKE_NOT_CREATE 반환")
 	void Given_ExistLike_When_registerLike_Then_LIKE_NOT_CREATE() throws Exception {
 		// given
-		Meme meme = memeRepository.save(MEME(member));
-		Memes memes = memesRepository.save(MEMES(member, meme));
 		likeRepository.save(LIKE(member, memes));
 
 		// when
@@ -91,5 +91,30 @@ public class MemesLikeIntegrationTest extends BaseIntegrationTest {
 
 		// then
 		assertThat(result).isEqualTo(LIKE_NOT_CREATE.getMessage());
+	}
+
+	@Test
+	@DisplayName("memesId를 통한 좋아요 삭제 성공")
+	void Given_memesId_When_cancelLike_Then_DELETE_LIKE_SUCCESS() {
+		// given
+		Like like = likeRepository.save(LIKE(member, memes));
+
+		// when
+		ExtractableResponse<Response> response =
+			given()
+				.log()
+				.all()
+				.auth().oauth2(accessToken)
+				.when()
+				.delete("/memes/{memesId}/like", memes.getId())
+				.then()
+				.log()
+				.all()
+				.extract();
+
+		String result = response.jsonPath().get(MESSAGE);
+
+		// then
+		assertThat(result).isEqualTo(DELETE_LIKE_SUCCESS.getMessage());
 	}
 }
