cd20d2cb0b5802738d33e688f3e23b0db9fa2d09
JunRain <sa2313@tukorea.ac.kr>
2024-06-13 19:45:06 +0900

    [#94] MemesIntegrationTest 밈스 생성 성공 테스트 구현

---
+++ backend/memetory/src/test/java/com/example/memetory/domain/memes/MemesIntegrationTest.java
@@ -0,0 +1,58 @@
+package com.example.memetory.domain.memes;
+
+import static com.example.memetory.domain.meme.MemeFixture.*;
+import static io.restassured.RestAssured.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+import com.example.memetory.domain.meme.entity.Meme;
+import com.example.memetory.domain.meme.repository.MemeRepository;
+import com.example.memetory.domain.memes.dto.request.GenerateMemesRequest;
+import com.example.memetory.domain.memes.dto.response.MemesResponse;
+import com.example.memetory.domain.memes.repository.MemesRepository;
+import com.example.memetory.global.integration.BaseIntegrationTest;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+@DisplayName("Memes 통합 테스트의 ")
+public class MemesIntegrationTest extends BaseIntegrationTest {
+	@Autowired
+	private MemeRepository memeRepository;
+	@Autowired
+	private MemesRepository memesRepository;
+
+	@Test
+	@DisplayName("GenerateMemesRequest을 통한 Memes 생성 성공")
+	public void Given_GenerateMemesRequest_When_registerMemes_Then_MemesResponse() {
+		// given
+		Meme meme = memeRepository.save(MEME(member));
+
+		final String title = "new Memes";
+		GenerateMemesRequest request = new GenerateMemesRequest(meme.getId(), title);
+
+		// when
+		ExtractableResponse<Response> response =
+			given()
+				.log()
+				.all()
+				.auth().oauth2(accessToken)
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.body(request)
+				.when()
+				.post("/memes")
+				.then()
+				.log()
+				.all()
+				.extract();
+
+		MemesResponse result = response.jsonPath().getObject("data", MemesResponse.class);
+
+		// then
+		assertThat(result.getTitle()).isEqualTo(title);
+	}
+}
