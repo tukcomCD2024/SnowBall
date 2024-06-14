5133f4869d51ae0f589e53c095e86f7da42fad6f
JunRain <sa2313@tukorea.ac.kr>
2024-06-14 17:51:06 +0900

    [#59] CommentController를 MemesCommentController로 변경

--- backend/memetory/src/main/java/com/example/memetory/domain/memes/controller/comment/MemesCommentController.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/memes/controller/comment/MemesCommentController.java
@@ -1,4 +1,4 @@
-package com.example.memetory.domain.comment.controller;
+package com.example.memetory.domain.memes.controller.comment;
 
 import static com.example.memetory.global.response.ResultCode.*;
 
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.memetory.domain.comment.dto.CommentServiceDto;
-import com.example.memetory.domain.comment.dto.request.GenerateCommentRequest;
+import com.example.memetory.domain.comment.dto.CommentInfo;
+import com.example.memetory.domain.comment.dto.request.CommentRequest;
 import com.example.memetory.domain.comment.service.CommentService;
 import com.example.memetory.global.annotation.LoginMemberEmail;
 import com.example.memetory.global.response.ResultResponse;
@@ -20,28 +20,32 @@ import com.example.memetory.global.response.ResultResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/comments")
 @RequiredArgsConstructor
-public class CommentController implements CommentApi {
-
+@RequestMapping("/memes/{memesId}/comments")
+public class MemesCommentController implements MemesCommentApi {
 	private final CommentService commentService;
 
 	@PostMapping
 	@Override
-	public ResponseEntity<ResultResponse> register(@LoginMemberEmail String email,
-		@RequestBody GenerateCommentRequest generateCommentRequest) {
-		CommentServiceDto newCommentServiceDto = generateCommentRequest.toServiceDto(email);
-		commentService.register(newCommentServiceDto);
+	public ResponseEntity<ResultResponse> registerComment(
+		@LoginMemberEmail String email,
+		@PathVariable Long memesId,
+		@RequestBody CommentRequest commentRequest
+	) {
+		commentRequest.setMemesIdAndEmail(memesId, email);
+		CommentInfo response = commentService.saveComment(commentRequest);
 
-		return ResponseEntity.status(HttpStatus.CREATED).body(ResultResponse.of(CREATE_COMMENT_SUCCESS));
+		return ResponseEntity.status(HttpStatus.CREATED).body(ResultResponse.of(CREATE_COMMENT_SUCCESS, response));
 	}
 
 	@DeleteMapping("/{commentId}")
 	@Override
-	public ResponseEntity<ResultResponse> delete(@PathVariable Long commentId) {
-		CommentServiceDto newCommentServiceDto = CommentServiceDto.create(commentId);
-		commentService.delete(newCommentServiceDto);
+	public ResponseEntity<ResultResponse> deleteComment(@LoginMemberEmail String email, @PathVariable Long commentId) {
+		commentService.deleteComment(email, commentId);
 
 		return ResponseEntity.ok(ResultResponse.of(DELETE_COMMENT_SUCCESS));
 	}
+
+	//TODO 조회(슬라이스 형식)
+
 }
