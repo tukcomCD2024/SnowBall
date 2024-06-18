537466df802f42469589f5cb046dd929dea74215
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2024-06-18 15:27:11 +0900

    [#86] VoiceSelectViewController 추가

--- iOS/Memetory/Memetory/View/Meme-generate/Voice/VoiceSelectViewController.swift
+++ iOS/Memetory/Memetory/View/Meme-generate/Voice/VoiceSelectViewController.swift
@@ -7,10 +7,32 @@
 
 import UIKit
 import SnapKit
+import AWSS3
+import AVFAudio
 
 class VoiceSelectViewController: UIViewController {
     
-    private let tableView =  UITableView()
+    let S3BucketName = "memetory"
+    let voiceUploadManager = VoiceManager()
+    private let voiceDemo = Voice.voiceDemo
+    
+    
+//    private let tableView =  UITableView()
+    lazy var tableView = UITableView()
+    
+    let uploadButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("나의 업로드", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 18)
+        button.layer.cornerRadius = 10
+        button.layer.masksToBounds = true
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.backgroundColor = UIColor.black.cgColor
+        button.addTarget(self, action: #selector(uploadButtonTapped), for: .touchUpInside)
+        return button
+    }()
     
     let checkButton: UIButton = {
         let button = UIButton(type: .custom)
@@ -36,13 +58,12 @@ class VoiceSelectViewController: UIViewController {
         return button
     }()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         title = "목소리 선택하기"
         view.backgroundColor = .white
-
+        
         setupTableView()
         setViews()
         setConstraints()
@@ -50,6 +71,7 @@ class VoiceSelectViewController: UIViewController {
     
     func setViews() {
         view.addSubview(tableView)
+        view.addSubview(uploadButton)
         view.addSubview(checkButton)
     }
     
@@ -60,6 +82,13 @@ class VoiceSelectViewController: UIViewController {
             make.trailing.equalToSuperview().offset(-10)
             make.bottom.equalTo(checkButton.snp.top).offset(20)
             
+        }
+        
+        uploadButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.width.equalTo(320)
+            make.height.equalTo(45)
+            make.bottom.equalTo(checkButton.snp.top).offset(-30)
         }
         
         checkButton.snp.makeConstraints { make in
@@ -73,25 +102,67 @@ class VoiceSelectViewController: UIViewController {
     func setupTableView() {
         tableView.dataSource = self
         tableView.delegate = self
-        
         tableView.rowHeight = 60
-        
         tableView.register(VoiceTableViewCell.self, forCellReuseIdentifier: "VoiceSelectCell")
     }
     
+    
+    @objc func uploadButtonTapped() {
+        let audioPickerController = UIImagePickerController()
+        audioPickerController.delegate = self
+        audioPickerController.sourceType = .savedPhotosAlbum
+        audioPickerController.mediaTypes = ["public.audio"]
+        present(audioPickerController, animated: true, completion: nil)
+    }
+    
     @objc func checkButtonTapped() {
+        let loadingVC = LoadingViewController()
+        navigationController?.pushViewController(loadingVC, animated: true)
         
+//        let SelectVC = SelectViewController()
+//        navigationController?.pushViewController(SelectVC, animated: true)
     }
 }
 
 extension VoiceSelectViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return voiceDemo.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "VoiceSelectCell", for: indexPath) as! VoiceTableViewCell
         cell.selectionStyle = .none
+        cell.nickNameLabel.text = voiceDemo[indexPath.item]
         return cell
+    }
+}
+
+extension VoiceSelectViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let audioURL = info[UIImagePickerController.InfoKey.mediaURL] as? URL else {
+            print("Error: No audio selected")
+            return
+        }
+        
+        uploadVoice(audioURL)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func uploadVoice(_ audioURL: URL) {
+        voiceUploadManager.uploadVoiceToS3(audioURL) { (voiceURLString) in
+            if let voiceURLString = voiceURLString {
+                print("Uploaded voice URL: \(voiceURLString)")
+                // 업로드 성공 시 여기에서 추가 작업을 수행할 수 있습니다.
+            } else {
+                print("Failed to upload voice.")
+                // 업로드 실패 시 여기에서 처리할 수 있는 로직을 추가하세요.
+            }
+        }
     }
 }
