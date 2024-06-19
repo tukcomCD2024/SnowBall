7e3be4e20eac4e8fd1108a3d06e334b93a154974
youngseo3 <wys4475@gmail.com>
2024-06-19 23:54:19 +0900

    [#96] VideoRVAdapter
    DiffUtil로 수정

--- Android/app/src/main/java/com/snowball/memetory/presentation/ui/locker/adapter/VideoRVAdapter.kt
+++ Android/app/src/main/java/com/snowball/memetory/presentation/ui/locker/adapter/VideoRVAdapter.kt
@@ -7,39 +7,6 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.snowball.memetory.databinding.ItemLockerVideoBinding
 
-//class VideoRVAdapter(private val onItemClick: (String) -> Unit) :
-//    RecyclerView.Adapter<VideoRVAdapter.ViewHolder>() {
-//
-//    var data = listOf<String>() // URL 리스트
-//
-//    fun updateData(newData: List<String>) {
-//        data = newData
-//        notifyDataSetChanged()  // 데이터가 변경되면 리사이클러뷰를 업데이트합니다.
-//    }
-//
-//    class ViewHolder(private val binding: ItemLockerVideoBinding) : RecyclerView.ViewHolder(binding.root) {
-//        fun bind(url: String, onClick: (String) -> Unit) {
-//            binding.thumnailImg.setOnClickListener {
-//                onClick(url)
-//            }
-//            // 이미지 로딩 로직 구현
-//        }
-//    }
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-//        val binding = ItemLockerVideoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-//        return ViewHolder(binding)
-//    }
-//
-//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        val url = data[position]
-//        holder.bind(url, onItemClick)
-//    }
-//
-//    override fun getItemCount() = data.size
-//}
-
-
 class VideoRVAdapter(private val onVideoClick: (String) -> Unit) :
     ListAdapter<String, VideoRVAdapter.VideoViewHolder>(DiffCallback) {
 
