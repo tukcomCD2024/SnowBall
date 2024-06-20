0791f9522de621b99983bcf08371160761e4fd41
youngseo3 <wys4475@gmail.com>
2024-06-20 16:32:33 +0900

    [#96] LockerService 수정
    밈's 생성 API를 위한

--- Android/app/src/main/java/com/snowball/memetory/data/api/LockerService.kt
+++ Android/app/src/main/java/com/snowball/memetory/data/api/LockerService.kt
@@ -1,14 +1,22 @@
 package com.snowball.memetory.data.api
 
 import com.snowball.memetory.data.dto.ResponseBody
+import com.snowball.memetory.data.dto.generatememe.meme.request.GenerateMemeRequestDto
+import com.snowball.memetory.data.dto.locker.request.GenerateMemesRequestDto
 import com.snowball.memetory.data.dto.locker.response.GetAllMemeResponseDto
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface LockerService {
     // 밈 전체 조회
     @GET("/meme")
     suspend fun getAllMeme(@Query("page") page: Int, @Query("size") size: Int): Response<ResponseBody<GetAllMemeResponseDto>>
+
+    // 밈's 생성
+    @POST("/memes")
+    suspend fun generateMemes(@Body generateMemesRequestDto: GenerateMemesRequestDto): Response<ResponseBody<Unit>>
 
 }
