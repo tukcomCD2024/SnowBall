ce9a5cdadd9d8b6450c1d225122e3c31ffef2a0a
youngseo3 <wys4475@gmail.com>
2024-06-20 17:36:55 +0900

    [#96] GenerateMemeFragment 수정
    그리드 레이아웃으로 수정

--- Android/app/src/main/java/com/snowball/memetory/presentation/ui/generatememe/GenerateMemeFragment.kt
+++ Android/app/src/main/java/com/snowball/memetory/presentation/ui/generatememe/GenerateMemeFragment.kt
@@ -2,42 +2,58 @@ package com.snowball.memetory.presentation.ui.generatememe
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.snowball.memetory.R
+import com.snowball.memetory.data.api.NetworkModule
+import com.snowball.memetory.data.repository.GenerateMemeRepository
 import com.snowball.memetory.databinding.FragmentGenerateMemeBinding
 import com.snowball.memetory.presentation.ui.generatememe.adapter.TemplateRVAdater
+import com.snowball.memetory.presentation.ui.generatememe.scenedetail.SceneDetailViewModel
+import com.snowball.memetory.presentation.ui.generatememe.scenedetail.SceneDetailViewModelFactory
 
 class GenerateMemeFragment : Fragment(), TemplateRVAdater.OnItemClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var viewModel: SceneDetailViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentGenerateMemeBinding = DataBindingUtil.inflate(inflater,
-                                                    R.layout.fragment_generate_meme,
-                                                    container,
-                                                    false)
-        val arrayList = arrayListOf<Int>(3)
-        binding.templateRecyclerView.adapter = TemplateRVAdater(arrayList, this)
+        val binding: FragmentGenerateMemeBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_generate_meme, container, false
+        )
+        val generateMemeService = NetworkModule.generateMemeService
+        val generateMemeRepository = GenerateMemeRepository(generateMemeService)
+        val viewModelFactory = SceneDetailViewModelFactory(generateMemeRepository)
+        viewModel = ViewModelProvider(requireActivity(), viewModelFactory).get(SceneDetailViewModel::class.java)
+
+        // Assuming you have 3 items and you want each row to have 2 columns
+        val gridLayoutManager = GridLayoutManager(context, 2)
+        binding.templateRecyclerView.layoutManager = gridLayoutManager
+
+        // Example list of images (You might want to replace these with actual drawable resource IDs)
+        val imageList = arrayListOf(R.drawable.demon1, R.drawable.world1, R.drawable.horseking1)
+        binding.templateRecyclerView.adapter = TemplateRVAdater(imageList, this)
 
         return binding.root
-        
     }
 
     override fun onItemClick(view: View, position: Int) {
+        viewModel.setSelectedTemplateIndex(position)
+        Log.e("GenerateMemeFragment", "템플릿 클릭된 position값(selectedTemplateIndex): $position")
         val intent = Intent(activity, GenerateMemeActivity::class.java)
-        // 필요한 경우 intent에 데이터 추가
-        // intent.putExtra("extra_data", value)
+        intent.putExtra("selectedTemplateIndex", viewModel.selectedTemplateIndex.value)
+
+        // Intent data can be added here if needed
         startActivity(intent)
     }
 
