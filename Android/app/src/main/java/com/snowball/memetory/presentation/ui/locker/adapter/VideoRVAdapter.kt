7bd3d96715a2eac7c8bad6ef6a9c45595eeb1e11
youngseo3 <wys4475@gmail.com>
2024-06-20 17:35:54 +0900

    [#96] VideoRVAdpater 수정
    DiffUtil과 ListAdapter 사용

--- Android/app/src/main/java/com/snowball/memetory/presentation/ui/locker/adapter/VideoRVAdapter.kt
+++ Android/app/src/main/java/com/snowball/memetory/presentation/ui/locker/adapter/VideoRVAdapter.kt
@@ -1,14 +1,21 @@
 package com.snowball.memetory.presentation.ui.locker.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.snowball.memetory.databinding.ItemLockerVideoBinding
+import com.snowball.memetory.domain.model.locker.Video
+import com.snowball.memetory.presentation.ui.locker.VideoOptionsBottomSheetFragment
 
-class VideoRVAdapter(private val onVideoClick: (String) -> Unit) :
-    ListAdapter<String, VideoRVAdapter.VideoViewHolder>(DiffCallback) {
+
+class VideoRVAdapter(
+    private val fragmentManager: FragmentManager,
+    private val onVideoClick: (String) -> Unit
+) : ListAdapter<Video, VideoRVAdapter.VideoViewHolder>(DiffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoViewHolder {
         val binding = ItemLockerVideoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -16,19 +23,53 @@ class VideoRVAdapter(private val onVideoClick: (String) -> Unit) :
     }
 
     override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
-        val videoUrl = getItem(position)
-        holder.bind(videoUrl)
+        holder.bind(getItem(position))
     }
 
     inner class VideoViewHolder(private val binding: ItemLockerVideoBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(videoUrl: String) {
-            binding.thumnailImg.setOnClickListener { onVideoClick(videoUrl) }
-            // Set thumbnail image using Glide or another library
+        fun bind(videoData: Video) {
+            binding.thumnailImg.setOnClickListener { onVideoClick(videoData.s3Url) }
+            binding.settingImg.setOnClickListener {
+                Log.d("VideoRVAdapter", "Meme ID for settings click: ${videoData.memeId}")
+
+                VideoOptionsBottomSheetFragment.newInstance(videoData.s3Url, videoData.memeId).show(fragmentManager, "videoOptions")
+            }
         }
     }
 
-    companion object DiffCallback : DiffUtil.ItemCallback<String>() {
-        override fun areItemsTheSame(oldItem: String, newItem: String) = oldItem == newItem
-        override fun areContentsTheSame(oldItem: String, newItem: String) = oldItem == newItem
+    companion object DiffCallback : DiffUtil.ItemCallback<Video>() {
+        override fun areItemsTheSame(oldItem: Video, newItem: Video): Boolean = oldItem.s3Url == newItem.s3Url
+        override fun areContentsTheSame(oldItem: Video, newItem: Video): Boolean = oldItem == newItem
     }
 }
+
+
+//class VideoRVAdapter(private val fragmentManager: FragmentManager,
+//                     private val onVideoClick: (String) -> Unit) :
+//    ListAdapter<String, VideoRVAdapter.VideoViewHolder>(DiffCallback) {
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoViewHolder {
+//        val binding = ItemLockerVideoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+//        return VideoViewHolder(binding)
+//    }
+//
+//    override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
+//        val videoUrl = getItem(position)
+//        holder.bind(videoUrl)
+//    }
+//
+//    inner class VideoViewHolder(private val binding: ItemLockerVideoBinding) : RecyclerView.ViewHolder(binding.root) {
+//        fun bind(videoUrl: String) {
+//            binding.thumnailImg.setOnClickListener { onVideoClick(videoUrl) }
+//            binding.settingImg.setOnClickListener {
+//                VideoOptionsBottomSheetFragment.newInstance(videoUrl).show(fragmentManager, "videoOptions")
+//            }
+//
+//        }
+//    }
+//
+//    companion object DiffCallback : DiffUtil.ItemCallback<String>() {
+//        override fun areItemsTheSame(oldItem: String, newItem: String) = oldItem == newItem
+//        override fun areContentsTheSame(oldItem: String, newItem: String) = oldItem == newItem
+//    }
+//}
