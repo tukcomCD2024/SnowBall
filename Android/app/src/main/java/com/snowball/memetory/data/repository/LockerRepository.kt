f7c20557d57cae4d69978d1579ea06654d781b13
youngseo3 <wys4475@gmail.com>
2024-06-20 17:04:01 +0900

    [#96] LockerRepository 수정
    generateMemes 밈's 생성 추가

--- Android/app/src/main/java/com/snowball/memetory/data/repository/LockerRepository.kt
+++ Android/app/src/main/java/com/snowball/memetory/data/repository/LockerRepository.kt
@@ -2,6 +2,7 @@ package com.snowball.memetory.data.repository
 
 import com.snowball.memetory.data.api.LockerService
 import com.snowball.memetory.data.dto.ResponseBody
+import com.snowball.memetory.data.dto.locker.request.GenerateMemesRequestDto
 import com.snowball.memetory.data.dto.locker.response.GetAllMemeResponseDto
 
 class LockerRepository(private val lockerService: LockerService) {
@@ -12,6 +13,19 @@ class LockerRepository(private val lockerService: LockerService) {
                 Result.success(response.body()!!)
             } else {
                 Result.failure(RuntimeException("Failed to load memes"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun generateMemes(request: GenerateMemesRequestDto): Result<ResponseBody<Unit>> {
+        return try {
+            val response = lockerService.generateMemes(request)
+            if (response.isSuccessful) {
+                Result.success(response.body()!!)
+            } else {
+                Result.failure(RuntimeException("Failed to 밈's 생성"))
             }
         } catch (e: Exception) {
             Result.failure(e)
