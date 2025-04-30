8cefde0371bf41b043a57bf034d5ade0569d3879
JunRain <sa2313@tukorea.ac.kr>
2025-04-30 20:28:13 +0900

    [refactor/#114] Redis에 데이터가 없는 경우 조회하기 위한 쿼리 구현

--- backend/memetory/src/main/java/com/example/memetory/domain/memes/repository/MemesRepository.java
+++ backend/memetory/src/main/java/com/example/memetory/domain/memes/repository/MemesRepository.java
@@ -1,8 +1,12 @@
 package com.example.memetory.domain.memes.repository;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.memetory.domain.memes.entity.Memes;
 
 public interface MemesRepository extends JpaRepository<Memes, Long>, MemesQueryRepository {
+	List<Memes> findMemesByIdIn(Collection<Long> ids);
 }
